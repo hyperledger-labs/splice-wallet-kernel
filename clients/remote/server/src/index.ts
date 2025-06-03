ce65a97218e8a837e724fc4af68a9c4f402ed403
rukmini-basu-da <126689545+rukmini-basu-da@users.noreply.github.com>
2025-06-03 15:17:44 -0400

    boiler plate express server (#27)
    * boiler plate server
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * use ES modules rather than CommonJS
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * use tsx rather than ts-node
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    ---------
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>

---
+++ clients/remote/server/src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/api/hello', (req, res) => {
+  res.json({message: 'Hello, world!'});
+});
+
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
