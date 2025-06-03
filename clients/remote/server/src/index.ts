332974bfacf4c8e7b3eea133a24d5ba71dcdf9c7
rukmini-basu-da <rukmini.basu@digitalasset.com>
2025-06-03 19:13:24 +0000

    use ES modules rather than CommonJS
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>

--- clients/remote/server/src/index.ts
+++ clients/remote/server/src/index.ts
@@ -1,20 +1,13 @@
-import express, {Request, Response} from 'express';
+import express from 'express';
 
 const app = express();
 const port = 3000;
 
-app.use(express.json());
-
-app.get('/api/hello', (req: Request, res: Response) => {
-    res.json({message: 'Hello, world!'});
+app.get('/api/hello', (req, res) => {
+  res.json({message: 'Hello, world!'});
 });
 
-app.post('/api/data', (req: Request, res: Response) => {
-    const data = req.body;
-    console.log('Received data:', data);
-    res.json({message: 'Data received', data});
-});
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
