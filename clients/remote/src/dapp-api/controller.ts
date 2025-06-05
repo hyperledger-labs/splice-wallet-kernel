b2c2ff9a5b93d5fef64aeaf181e41cea6d37b053
Alex Matson <alex.matson@digitalasset.com>
2025-06-05 19:55:10 -0400

    update
    Signed-off-by: Alex Matson <alex.matson@digitalasset.com>

--- clients/remote/src/dapp-api/controller.ts
+++ clients/remote/src/dapp-api/controller.ts
@@ -1,20 +1,14 @@
 import buildController from './rpc-gen/index.js'
 
 export const dappController = buildController({
-    addNetwork: async (params) => null,
-    allocateParty: async (params) =>
-        Promise.resolve({ partyId: 'default-party-id' }),
-    connect: async () => Promise.resolve({ chainId: 'default-chain-id' }),
+    connect: async () =>
+        Promise.resolve({
+            chainId: 'default-chain-id',
+            userUrl: 'http://default-user-url.com',
+        }),
     darsAvailable: async () => Promise.resolve({ dars: ['default-dar'] }),
     ledgerApi: async (params) =>
         Promise.resolve({ response: 'default-response' }),
     prepareExecute: async (params) => null,
     prepareReturn: async (params) => Promise.resolve({}),
-    removeParty: async (params) => Promise.resolve({}),
-    sign: async (params) =>
-        Promise.resolve({
-            signature: 'default-signature',
-            party: 'default-party',
-            signedBy: 'default-signed-by',
-        }),
 })
