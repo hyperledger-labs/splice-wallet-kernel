6de31e0e45dd32758af8eb8842da955fe1510ef3
Alex Matson <alex.matson@digitalasset.com>
2025-06-12 07:33:34 -0400

    commands
    Signed-off-by: Alex Matson <alex.matson@digitalasset.com>

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+export const apps = [
+    {
+        name: 'remote',
+        script: 'yarn workspace server dev',
+    },
+    {
+        name: 'example dApp',
+        script: 'yarn workspace example dev',
+    },
+    {
+        name: 'sdk',
+        script: 'yarn workspace splice-wallet-sdk dev',
+    },
+]
