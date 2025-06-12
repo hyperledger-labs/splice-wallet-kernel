1727785ca118c83bf306d5abe77a5fd314d7fbdc
rukmini-basu-da <rukmini.basu@digitalasset.com>
2025-06-12 08:06:27 -0400

    add vite config alias to example DApp
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
+    resolve: {
+        alias: {
+            'core-wallet-dapp-rpc-client': path.resolve(
+                __dirname,
+                '../core/wallet-dapp-rpc-client'
+            ),
+        },
+    },
 })
