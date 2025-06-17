cd95ab7c9ec8e13149b831d20b37b677d6e56128
Alex Matson <alex.matson@digitalasset.com>
2025-06-17 15:20:07 -0400

    Run unit tests in CI (#91)
    Signed-off-by: Alex Matson <alex.matson@digitalasset.com>

--- clients/remote/src/index.test.ts
+++ clients/remote/src/index.test.ts
@@ -3,13 +3,28 @@ import { afterAll, expect, test } from '@jest/globals'
 import request from 'supertest'
 import { dAppServer } from './index.js'
 
-test('hello world', async () => {
-    const response = await request(dAppServer).get('/api/hello')
+test('call connect rpc', async () => {
+    const response = await request(dAppServer)
+        .post('/rpc')
+        .send({ jsonrpc: '2.0', id: 0, method: 'connect', params: [] })
+        .set('Accept', 'application/json')
 
     expect(response.statusCode).toBe(200)
-    expect(response.body).toEqual({ message: 'Hello, world!' })
+    expect(response.body).toEqual({
+        id: 0,
+        jsonrpc: '2.0',
+        result: {
+            chainId: 'default-chain-id',
+            userUrl: 'http://default-user-url.com',
+        },
+    })
 })
 
-afterAll(() => {
+beforeAll((done) => {
+    done()
+})
+
+afterAll((done) => {
     dAppServer.close()
+    done()
 })
