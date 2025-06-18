d9e99bba87b8aef785480d84622cf1975ee2a8cb
rukmini-basu-da <rukmini.basu@digitalasset.com>
2025-06-18 11:41:28 -0400

    load networks config server
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>

--- clients/remote/src/config/StoreConfig.test.ts
+++ clients/remote/src/config/StoreConfig.test.ts
@@ -1,59 +1,46 @@
 import { expect, test } from '@jest/globals'
 import * as s from './StoreConfig.js'
-import * as path from 'path'
-import { readFileSync } from 'fs'
+import { ConfigUtils } from './ConfigUtils.js'
 
 test('account config from json file', async () => {
-    const __dirname = path.resolve()
-
-    const filePath = path.join(__dirname, '../test/account-config.json')
-
-    const jsonData = JSON.parse(readFileSync(filePath, 'utf-8'))
+    const jsonData = ConfigUtils.loadConfigFile('../test/account-config.json')
     const resp = s.accountSchema.parse(jsonData)
     expect(resp.id).toBe('xyz')
     expect(resp.name).toBe('name1')
 })
 
 test('network config from json file', async () => {
-    const __dirname = path.resolve()
-
-    const filePath = path.join(__dirname, '../test/network-config.json')
-
-    const jsonData = JSON.parse(readFileSync(filePath, 'utf-8'))
+    const jsonData = ConfigUtils.loadConfigFile('../test/network-config.json')
     const resp = s.networkConfigSchema.parse(jsonData)
     expect(resp.name).toBe('xyz')
     expect(resp.ledgerApi.baseUrl).toBe('https://test')
-    expect(resp.authType.clientId).toBe('wk-service-account')
-    expect(resp.authType.scope).toBe('openid')
-    expect(resp.authType.type).toBe('password')
+    expect(resp.auth.clientId).toBe('wk-service-account')
+    expect(resp.auth.scope).toBe('openid')
+    expect(resp.auth.type).toBe('password')
 })
 
 test('multiple network config from json file', async () => {
-    const __dirname = path.resolve()
+    const jsonData = ConfigUtils.loadConfigFile(
+        '../test/multi-network-config.json'
+    )
+    const resp = s.networksSchema.parse(jsonData)
 
-    const filePath = path.join(__dirname, '../test/multi-network-config.json')
-
-    const jsonData = JSON.parse(readFileSync(filePath, 'utf-8'))
-    const resp = s.networks.parse(jsonData)
-
-    expect(resp[0].authType.type).toBe('password')
-    if (resp[0].authType.type === 'password') {
-        expect(resp[0].authType.tokenUrl).toBe('tokenUrl')
+    expect(resp[0].auth.type).toBe('password')
+    if (resp[0].auth.type === 'password') {
+        expect(resp[0].auth.tokenUrl).toBe('tokenUrl')
     }
-    expect(resp[1].authType.type).toBe('implicit')
-    if (resp[1].authType.type === 'implicit') {
-        expect(resp[1].authType.audience).toBe(
+    expect(resp[1].auth.type).toBe('implicit')
+    if (resp[1].auth.type === 'implicit') {
+        expect(resp[1].auth.audience).toBe(
             'https://daml.com/jwt/aud/participant/wk-app'
         )
     }
 })
 
 test('multiple account config from json file', async () => {
-    const __dirname = path.resolve()
-
-    const filePath = path.join(__dirname, '../test/multi-account-config.json')
-
-    const jsonData = JSON.parse(readFileSync(filePath, 'utf-8'))
+    const jsonData = ConfigUtils.loadConfigFile(
+        '../test/multi-account-config.json'
+    )
     const resp = s.accounts.parse(jsonData)
 
     expect(resp.length).toBe(2)
