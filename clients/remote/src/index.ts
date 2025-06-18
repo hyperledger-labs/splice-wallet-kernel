a7d0d4f584a5f5842830b71853e1db5992f6ad6e
Alex Matson <alex.matson@digitalasset.com>
2025-06-18 08:43:29 -0400

    Integrate a Vite dev server as an Express middleware
    Signed-off-by: Alex Matson <alex.matson@digitalasset.com>

--- clients/remote/src/index.ts
+++ clients/remote/src/index.ts
@@ -2,6 +2,7 @@ import { dapp } from './dapp-api/server.js'
 import { user } from './user-api/server.js'
 import { web } from './web/server.js'
 import { pino } from 'pino'
+import ViteExpress from 'vite-express'
 
 const dAppPort = 3000
 const userPort = 3001
@@ -17,6 +18,6 @@ export const userServer = user.listen(userPort, () => {
     logger.info(`User Server running at http://localhost:${userPort}`)
 })
 
-export const webServer = web.listen(webPort, () => {
+export const webServer = ViteExpress.listen(web, webPort, () =>
     logger.info(`Web server running at http://localhost:${webPort}`)
-})
+)
