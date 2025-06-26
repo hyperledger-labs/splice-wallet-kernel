ed3b8da0f06a7210f87cc8b80879186e354335b2
rukmini-basu-da <rukmini.basu@digitalasset.com>
2025-06-26 15:27:00 -0400

    login
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>

--- clients/remote/src/web/server.ts
+++ clients/remote/src/web/server.ts
@@ -1,3 +1,16 @@
 import express from 'express'
+import { fileURLToPath } from 'url'
+import path from 'path'
 
 export const web = express()
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const mainApp = path.resolve(__dirname, './frontend/dist/app')
+const loginDir = path.resolve(__dirname, './frontend/dist/login')
+const callback = path.resolve(__dirname, './frontend/dist/callback')
+
+web.use('/', express.static(mainApp))
+web.use('/login', express.static(loginDir))
+web.use('/callback', express.static(callback))
