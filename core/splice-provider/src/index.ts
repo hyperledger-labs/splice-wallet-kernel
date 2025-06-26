d788e34833fae957a36c68cbdee781ac23e3dd0c
Marc Juchli <120378272+mjuchli-da@users.noreply.github.com>
2025-06-26 10:22:31 +0200

    Multiple SpliceProvider implementations (#113)
    Signed-off-by: Marc Juchli <marc.juchli@digitalasset.com>

--- core/splice-provider/src/index.ts
+++ core/splice-provider/src/index.ts
@@ -1,4 +1,6 @@
 import { SpliceProvider } from './SpliceProvider'
+import { SpliceProviderHttp } from './SpliceProviderHttp'
+import { SpliceProviderWindow } from './SpliceProviderWindow'
 
 declare global {
     interface Window {
@@ -8,13 +10,35 @@ declare global {
     }
 }
 
-export function injectSpliceProvider(): SpliceProvider {
+export enum ProviderType {
+    WINDOW,
+    HTTP,
+}
+
+export function injectSpliceProvider(
+    providerType: ProviderType,
+    url?: URL
+): SpliceProvider {
     // Check if the provider is already injected
     const existing = window.splice || window.canton || window.ethereum
     if (existing) return existing
 
     // Inject the SpliceProvider instance
-    window.splice = new SpliceProvider()
+    if (providerType === ProviderType.WINDOW) {
+        window.splice = new SpliceProviderWindow()
+    } else if (providerType === ProviderType.HTTP) {
+        if (!url || !(url instanceof URL)) {
+            throw new Error(
+                'Invalid URL for HTTP provider. Please provide a valid URL instance.'
+            )
+        }
+        window.splice = new SpliceProviderHttp(url)
+    } else {
+        throw new Error(
+            'Invalid provider type. Use ProviderType.WINDOW or ProviderType.HTTP.'
+        )
+    }
+
     window.canton = window.splice // For compatibility with Canton dApps
     window.ethereum = window.splice // For EIP-1193 compatibility
 
