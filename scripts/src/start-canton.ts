15e02e03e9143b4c9f6f70ed6d063e38899ca33b
phillip olesen <phillip.olesen@digitalasset.com>
2025-07-09 10:31:41 +0200

    added log output from starting canton to console
    Signed-off-by: phillip olesen <phillip.olesen@digitalasset.com>

--- scripts/src/start-canton.ts
+++ scripts/src/start-canton.ts
@@ -8,10 +8,12 @@ import {
     CANTON_CONF,
     error,
     info,
+    trimNewline,
 } from './utils.js'
 import { existsSync } from 'fs'
 import pm2 from 'pm2'
 
+const processName = 'canton'
 async function main() {
     if (existsSync(CANTON_BIN)) {
         console.log(
@@ -25,12 +27,22 @@ async function main() {
 
             pm2.start(
                 {
-                    name: 'canton',
+                    name: processName,
                     interpreter: '/bin/bash',
                     script: CANTON_BIN,
-                    args: `daemon --no-tty --config ${CANTON_CONF} --bootstrap ${CANTON_BOOTSTRAP}`,
+                    args: `daemon --no-tty --config ${CANTON_CONF} --bootstrap ${CANTON_BOOTSTRAP} --log-level-stdout=INFO --log-level-canton=INFO`,
                 },
                 function (err) {
+                    pm2.launchBus((err, bus) => {
+                        bus.on('log:out', (packet) => {
+                            if (packet.process.name == processName)
+                                console.log(info(trimNewline(packet.data)))
+                        })
+                        bus.on('log:err', (packet) => {
+                            if (packet.process.name == processName)
+                                console.error(error(trimNewline(packet.data)))
+                        })
+                    })
                     if (err) {
                         console.error(err)
                         return pm2.disconnect()
