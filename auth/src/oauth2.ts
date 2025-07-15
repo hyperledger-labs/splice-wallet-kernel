f6c282e657c23d0a2debbb2106eb0c797e4a35e5
rukmini-basu-da <rukmini.basu@digitalasset.com>
2025-07-15 14:41:48 -0400

    modify config.json to use new mock oauth server
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>

--- auth/src/oauth2.ts
+++ auth/src/oauth2.ts
@@ -1,5 +1,5 @@
 import { OAuth2Server } from 'oauth2-mock-server'
-// import basicAuth from 'basic-auth'
+import basicAuth from 'basic-auth'
 
 async function main() {
     const server = new OAuth2Server()
@@ -10,17 +10,13 @@ async function main() {
     const service = server.service
 
     service.on('beforeTokenSigning', (token, req) => {
-        // const credentials = basicAuth(req);
-        // const clientId = credentials ? credentials.name : req.body.client_id;
-        // const aud = req.body.audience
-        // token.payload.sub = clientId
-        // token.payload.aud =  aud
         console.log(req.body)
+        const credentials = basicAuth(req)
         token.payload.iss = 'http://127.0.0.1:8889'
-        token.payload.sub = 'Cg9zZXJ2aWNlLmFjY291bnQSBWxvY2Fs'
-        token.payload.aud =
-            'https://daml.com/jwt/aud/participant/canton-utilities-app'
-        token.payload.scope = 'openid daml_ledger_api offline_access'
+        const clientId = credentials ? credentials.name : req.body.client_id
+        const aud = req.body.audience
+        token.payload.sub = clientId
+        token.payload.aud = aud
     })
 }
 
