3ac5f527c49cb23a622bc834412b94a94a98bb63
rukmini-basu-da <rukmini.basu@digitalasset.com>
2025-07-15 08:42:25 -0400

    add auth to canton pn
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    # Conflicts:
    #	canton-bootstrap.canton
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>

---
+++ auth/src/oauth2.ts
@@ -0,0 +1,26 @@
+import { OAuth2Server } from 'oauth2-mock-server'
+// import basicAuth from 'basic-auth'
+
+async function main() {
+    const server = new OAuth2Server()
+
+    await server.issuer.keys.generate('RS256')
+    await server.start(8888, 'localhost')
+
+    const service = server.service
+
+    service.on('beforeTokenSigning', (token, req) => {
+        // const credentials = basicAuth(req);
+        // const clientId = credentials ? credentials.name : req.body.client_id;
+        // const aud = req.body.audience
+        // token.payload.sub = clientId
+        // token.payload.aud =  aud
+        console.log(req.body)
+        token.payload.sub = 'Cg9zZXJ2aWNlLmFjY291bnQSBWxvY2Fs'
+        token.payload.aud =
+            'https://daml.com/jwt/aud/participant/canton-utilities-app'
+        token.payload.scope = 'openid daml_ledger_api offline_access'
+    })
+}
+
+main()
