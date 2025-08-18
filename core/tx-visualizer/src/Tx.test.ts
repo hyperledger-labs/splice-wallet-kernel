5f9ad2a0a9008081981f100042005abd9eab571e
rukmini-basu-da <rukmini.basu@digitalasset.com>
2025-08-18 14:23:17 -0400

    core txvsisualizer
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>

---
+++ core/tx-visualizer/src/Tx.test.ts
@@ -0,0 +1,24 @@
+import { expect, test } from '@jest/globals'
+import { PreparedTransaction } from './_gen/com/daml/ledger/api/v2/interactive/interactive_submission_service'
+import * as path from 'path'
+import { readFileSync } from 'fs'
+import { decodePreparedTransaction } from '.'
+import camelcaseKeys from 'camelcase-keys'
+
+test('decode a base 64 encoded prepared tx', async () => {
+    const base64EncodedPreparedTx =
+        'Cp8GCgMyLjESATAa8AUKATDCPukFCuYFCgMyLjESQjAwNTUwODAyZGRiMTYzNzFmNWZjNmQzNjRlNmNkNGIzMjgzYTllYjVjMGNjYjEyMWFlMzY4Y2RlYmJhZDBmYmY1NhoOQWRtaW5Xb3JrZmxvd3MiXgpAMmEzOGI5NjNmNmFiZjQ1Yjc2YzcwMmY5NzAwYmZkOTA2MDU1NTg3MmFmOTE1ZWY3ZjhmNjg3OTVlMmM4MzFiZBIUQ2FudG9uLkludGVybmFsLlBpbmcaBFBpbmcqtgJyswIKXgpAMmEzOGI5NjNmNmFiZjQ1Yjc2YzcwMmY5NzAwYmZkOTA2MDU1NTg3MmFmOTE1ZWY3ZjhmNjg3OTVlMmM4MzFiZBIUQ2FudG9uLkludGVybmFsLlBpbmcaBFBpbmcSDwoCaWQSCUIHcGluZ19pZBJdCglpbml0aWF0b3ISUDpOb3BlcmF0b3I6OjEyMjBkNDRmYzFjM2JhMGI1YmRmN2I5NTZlZTcxYmM5NGViZTJkMjMyNThkYzI2OGZkZjA4MjRmYmFlZmYyYzYxNDI0EmEKCXJlc3BvbmRlchJUOlJwYXJ0aWNpcGFudDE6OjEyMjBkNDRmYzFjM2JhMGI1YmRmN2I5NTZlZTcxYmM5NGViZTJkMjMyNThkYzI2OGZkZjA4MjRmYmFlZmYyYzYxNDI0Mk5vcGVyYXRvcjo6MTIyMGQ0NGZjMWMzYmEwYjViZGY3Yjk1NmVlNzFiYzk0ZWJlMmQyMzI1OGRjMjY4ZmRmMDgyNGZiYWVmZjJjNjE0MjQ6Tm9wZXJhdG9yOjoxMjIwZDQ0ZmMxYzNiYTBiNWJkZjdiOTU2ZWU3MWJjOTRlYmUyZDIzMjU4ZGMyNjhmZGYwODI0ZmJhZWZmMmM2MTQyNDpScGFydGljaXBhbnQxOjoxMjIwZDQ0ZmMxYzNiYTBiNWJkZjdiOTU2ZWU3MWJjOTRlYmUyZDIzMjU4ZGMyNjhmZGYwODI0ZmJhZWZmMmM2MTQyNCIiEiBr2qJTURRRnXWp6y1EyXQiB69cfe50kJw0eYN1UF39nhL1ARJ2Ck5vcGVyYXRvcjo6MTIyMGQ0NGZjMWMzYmEwYjViZGY3Yjk1NmVlNzFiYzk0ZWJlMmQyMzI1OGRjMjY4ZmRmMDgyNGZiYWVmZjJjNjE0MjQSJGYyZWM0ZDhmLWNjYzEtNDAyYi1iMjc4LTc1NTZmZGQyYjQxMhpMd2FsbGV0OjoxMjIwZTdiMjNlYTUyZWI1YzY3MmZiMGIxY2RiYzkxNjkyMmZmZWQzZGQ3Njc2YzIyM2E2MDU2NjQzMTVlMmQ0M2VkZCokYTMyODE2MmUtNzI4ZS00ZTA1LWFjNzgtYjM0ZjA3MDk4M2JhMK7Y9/LU944D'
+
+    const preparedTx = decodePreparedTransaction(base64EncodedPreparedTx)
+
+    const __dirname = path.resolve()
+    const resolvedFilePath = path.join(
+        __dirname,
+        '../tx-visualizer/create_ping_prepared_response.json'
+    )
+    const preapredTXJson = JSON.parse(readFileSync(resolvedFilePath, 'utf-8'))
+    const camelCasePreparedTx = camelcaseKeys(preapredTXJson, { deep: true })
+    const message = PreparedTransaction.fromJson(camelCasePreparedTx)
+
+    expect(message === preparedTx)
+})
--- scripts/src/generate-protobufs.ts
+++ scripts/src/generate-protobufs.ts
@@ -7,6 +7,7 @@ import { ensureDir, getRepoRoot, traverseDirectory } from './utils.js'
 const repoRoot = getRepoRoot()
 
 const outdir = `${repoRoot}/core/ledger-client/src/_proto`
+
 const roots = [
     `${repoRoot}/.canton/protobuf/community`,
     `${repoRoot}/.canton/protobuf/lib`,
@@ -35,6 +36,32 @@ function generateProtos() {
         process.exit(1)
     }
 }
+
+// function generateProtosWithPlugin() {
+//     const protocArgs = [
+//         '--plugin=protoc-gen-ts_proto=$(yarn bin protoc-gen-ts_proto)',
+//         `--ts_out=${outdir2}`,
+//         '--ts_opt=generate_dependencies',
+//         ...roots.map((roots2) => `-I${roots2}`)
+//     ]
+
+//     try {
+//         execFileSync('grpc_tools_node_protoc', protocArgs, { stdio: 'inherit' })
+//         console.log('Protobuf files generated successfully.')
+//     } catch (error) {
+//         console.error('Error generating protobuf files:', error)
+//         process.exit(1)
+//     }
+// }
+
+// yarn grpc_tools_node_protoc --plugin=protoc-gen-ts_proto=$(yarn bin protoc-gen-ts_proto) --ts_out=/Users/rukminibasu/Desktop/IdeaProjects/splice-wallet-kernel/core/ledger-client/src/_gen \
+// --ts_opt=generate_dependencies \
+//  -I /Users/rukminibasu/Desktop/IdeaProjects/splice-wallet-kernel/.canton/protobuf/ledger-api $(find /Users/rukminibasu/Desktop/IdeaProjects/splice-wallet-kernel/.canton/protobuf/ledger-api -type f -name '*.proto' ! -path /Users/rukminibasu/Desktop/IdeaProjects/splice-wallet-kernel/.canton/protobuf/ledger-api/com/daml/ledger/api/scalapb/package.proto) \
+// -I /Users/rukminibasu/Desktop/IdeaProjects/splice-wallet-kernel/.canton/protobuf/lib $(find /Users/rukminibasu/Desktop/IdeaProjects/splice-wallet-kernel/.canton/protobuf/lib -type f -name '*.proto')
+
+// /Users/rukminibasu/Desktop/IdeaProjects/splice-wallet-kernel/.canton/protobuf/ledger-api/**/*.proto
+
+// $(find .canton/protobuf/ledger-api -type f -name '*.proto' ! -path .canton/protobuf/ledger-api/com/daml/ledger/api/scalapb/package.proto)
 
 // The protobuf plugin we're using, @protobuf-ts/plugin, generates files with relative imports that do not include the .js extension.
 // This is required in ESModule projects using NodeJS (non-bundler mode).
