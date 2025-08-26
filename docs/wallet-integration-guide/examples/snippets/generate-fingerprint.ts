e288b280f3069bd88591dfaddd02c5839f18879f
Alex Matson <alex.matson@digitalasset.com>
2025-08-26 17:02:40 -0400

    chore: rename sdk packages (#291)
    * Rename sdk packages
    
    Signed-off-by: Alex Matson <alex.matson@digitalasset.com>
    
    * use tabs for docs
    
    Signed-off-by: Alex Matson <alex.matson@digitalasset.com>
    
    ---------
    
    Signed-off-by: Alex Matson <alex.matson@digitalasset.com>

--- docs/wallet-integration-guide/examples/snippets/generate-fingerprint.ts
+++ docs/wallet-integration-guide/examples/snippets/generate-fingerprint.ts
@@ -1,4 +1,4 @@
-import { TopologyController } from '@splice/sdk-wallet'
+import { TopologyController } from '@splice/wallet-sdk'
 const publicKey = 'your-public-key-here'
 // static method call
 const fingerPrint = TopologyController.createFingerprintFromPublicKey(publicKey)
