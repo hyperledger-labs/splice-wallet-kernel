c4d712b9c3a47c66d7ca558065f1c162b03f66d8
phillip olesen <phillip.olesen@digitalasset.com>
2025-08-27 15:35:37 +0200

    docs: added relevant third party dapp support
    Signed-off-by: phillip olesen <phillip.olesen@digitalasset.com>

---
+++ docs/wallet-integration-guide/examples/snippets/convert-transaction-to-json.ts
@@ -0,0 +1,32 @@
+import {
+    WalletSDKImpl,
+    localNetAuthDefault,
+    localNetLedgerDefault,
+    decodePreparedTransaction,
+    PreparedTransaction,
+} from '@splice/wallet-sdk'
+import { v4 } from 'uuid'
+
+const sdk = new WalletSDKImpl().configure({
+    logger: console,
+    authFactory: localNetAuthDefault,
+    ledgerFactory: localNetLedgerDefault,
+    topologyFactory: undefined, //these calls require no topology changes
+})
+
+const prepareExecuteParams = {
+    commands: {}, // this is of type JsCommand
+}
+
+const preparedTransaction = await sdk.userLedger?.prepareSubmission(
+    prepareExecuteParams.commands, //the incoming command
+    v4() //a unique deduplication id for this transaction
+)
+
+const decodedTransaction = decodePreparedTransaction(
+    preparedTransaction!.preparedTransaction!
+)
+
+const JsonTransaction = PreparedTransaction.toJson(decodedTransaction)
+
+// Here you can use your choice of JSON visualizer
---
+++ docs/wallet-integration-guide/examples/snippets/prepare-incoming-command.ts
@@ -0,0 +1,40 @@
+import {
+    WalletSDKImpl,
+    localNetAuthDefault,
+    localNetLedgerDefault,
+    signTransactionHash,
+    TopologyController,
+} from '@splice/wallet-sdk'
+import { v4 } from 'uuid'
+
+const sdk = new WalletSDKImpl().configure({
+    logger: console,
+    authFactory: localNetAuthDefault,
+    ledgerFactory: localNetLedgerDefault,
+    topologyFactory: undefined, //these calls require no topology changes
+})
+
+const prepareExecuteParams = {
+    commands: {}, // this is of type JsCommand
+}
+
+const preparedTransaction = await sdk.userLedger?.prepareSubmission(
+    prepareExecuteParams.commands, //the incoming command
+    v4() //a unique deduplication id for this transaction
+)
+
+const clientsPublicKey = 'clients-public-key-here'
+const clientsPrivateKey = 'client-private-key-here'
+
+const signature = signTransactionHash(
+    preparedTransaction?.preparedTransactionHash ?? '',
+    clientsPrivateKey
+)
+
+//if client calls ``prepareExecute`` then this is how they would call ``execute``
+await sdk.userLedger?.executeSubmission(
+    preparedTransaction!,
+    signature,
+    clientsPublicKey,
+    v4()
+)
