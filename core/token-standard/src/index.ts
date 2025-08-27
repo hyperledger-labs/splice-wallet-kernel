a77abc482cdfb6f226e2d64969be3cf8f2136fcb
Pawel Stepien <pawel.stepien@digitalasset.com>
2025-08-27 14:40:40 +0200

    fix(core-token-standard, core-ledger-client) Fix token standard exports dependency graph
    Signed-off-by: Pawel Stepien <pawel.stepien@digitalasset.com>

--- core/token-standard/src/index.ts
+++ core/token-standard/src/index.ts
@@ -1,2 +1,9 @@
 export * from './token-standard-client'
-export * from './token-standard-models-1.0.0/lib/Splice/Api/Token'
+export {
+    AllocationInstructionV1,
+    AllocationRequestV1,
+    AllocationV1,
+    HoldingV1,
+    MetadataV1,
+    TransferInstructionV1,
+} from './token-standard-models-1.0.0/lib/Splice/Api/Token'
