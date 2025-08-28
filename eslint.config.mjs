fe7635fce8b2fac4aad77671886477d2ae8e281b
Alex Matson <alex.matson@digitalasset.com>
2025-08-28 16:37:23 -0400

    Merge commitlint allowed scopes with nx projects
    Signed-off-by: Alex Matson <alex.matson@digitalasset.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,7 @@ export default defineConfig([
     includeIgnoreFile(gitignorePath),
     globalIgnores([
         '.yarn/',
+        '.commitlintrc.js',
         '**/dist/',
         '**/build/',
         '.pnp.*',
