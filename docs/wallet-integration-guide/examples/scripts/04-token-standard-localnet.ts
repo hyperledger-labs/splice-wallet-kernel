93b4d6926f174db89900345e78bcdbcd70a1dd8f
Marc Juchli <marc.juchli@digitalasset.com>
2025-09-01 01:27:06 +0200

    Transfer works Signed-off-by: Marc Juchli <marc.juchli@digitalasset.com>

--- docs/wallet-integration-guide/examples/scripts/04-token-standard-localnet.ts
+++ docs/wallet-integration-guide/examples/scripts/04-token-standard-localnet.ts
@@ -55,6 +55,50 @@ await sdk.userLedger
 
 sdk.tokenStandard?.setSynchronizerId(synchonizerId)
 
+// Node cannot resolve subdomain.localhost, therefore add the following mapping to your /etc/hosts
+// 127.0.0.1   scan.localhost
+sdk.tokenStandard?.setTransferFactoryRegistryUrl('http://scan.localhost:4000')
+
+// await sdk.tokenStandard?.tap(
+//     'http://wallet.localhost:2000/api/validator',
+//     1000000
+// )
+
+// TODO: get from Scan
+const amuletRulesAndOpenMiningRound = await sdk.tokenStandard?.createTransfer(
+    party!.partyId,
+    'marc::1220ee06a9947d6951889e3458a2e36f0421df008e750fd86187536b3d01bb63363c',
+    '100',
+    {
+        instrumentId: 'Amulet',
+        instrumentAdmin:
+            'DSO::122098544e6d707a02edee40ff295792b2b526fa30fa7a284a477041eb23d1d26763',
+    },
+    {}
+)
+
+if (!amuletRulesAndOpenMiningRound) {
+    throw new Error('Failed to get Amulet rules and open mining round')
+}
+const [_, disclosedContracts] = amuletRulesAndOpenMiningRound
+
+const tapCommand = sdk.tokenStandard?.createTap(
+    disclosedContracts,
+    party!.partyId,
+    '2000000'
+)
+
+logger.debug({ ExerciseCommand: tapCommand }, 'tap command is')
+
+await sdk.userLedger?.prepareSignAndExecuteTransaction(
+    [{ ExerciseCommand: tapCommand }],
+    keyPair.privateKey,
+    v4(),
+    disclosedContracts
+)
+
+await new Promise((res) => setTimeout(res, 5000))
+
 logger.info('List Token Standard Holding Transactions')
 await sdk.tokenStandard
     ?.listHoldingTransactions()
@@ -71,15 +115,6 @@ await sdk.tokenStandard
         )
     })
 
-// Node cannot resolve subdomain.localhost, therefore add the following mapping to your /etc/hosts
-// 127.0.0.1   scan.localhost
-sdk.tokenStandard?.setTransferFactoryRegistryUrl('http://scan.localhost:4000')
-
-// await sdk.tokenStandard?.tap(
-//     'http://wallet.localhost:2000/api/validator',
-//     1000000
-// )
-
 const createTransfer = await sdk.tokenStandard?.createTransfer(
     party!.partyId,
     'marc::1220ee06a9947d6951889e3458a2e36f0421df008e750fd86187536b3d01bb63363c',
@@ -95,24 +130,7 @@ const createTransfer = await sdk.tokenStandard?.createTransfer(
 if (!createTransfer) {
     throw new Error('Failed to create transfer command')
 }
-const [transferCommand, disclosedContracts] = createTransfer
-
-const tapCommand = sdk.tokenStandard?.createTap(
-    disclosedContracts,
-    party!.partyId,
-    '2000000'
-)
-
-console.log('command is: ', tapCommand)
-
-await sdk.userLedger?.prepareSignAndExecuteTransaction(
-    [{ ExerciseCommand: tapCommand }],
-    keyPair.privateKey,
-    v4(),
-    disclosedContracts
-)
-
-console.log('command is: ', transferCommand)
+const [transferCommand, disclosedContracts2] = createTransfer
 
 const exerciseCommand =
     // const prepared = await sdk.userLedger?.prepareSubmission([exerciseCommand], v4(), disclosedContracts)
@@ -121,5 +139,5 @@ const exerciseCommand =
         [{ ExerciseCommand: transferCommand }],
         keyPair.privateKey,
         v4(),
-        disclosedContracts
+        disclosedContracts2
     )
