9d4819149f1d95ac03bd4f4178cc5746eed87934
Marc Juchli <marc.juchli@digitalasset.com>
2025-09-01 16:49:52 +0200

    Remove copy/paste
    Signed-off-by: Marc Juchli <marc.juchli@digitalasset.com>

--- core/scan-client/src/index.ts
+++
@@ -1,9 +0,0 @@
-export * from './token-standard-client.js'
-export type {
-    AllocationInstructionV1,
-    AllocationRequestV1,
-    AllocationV1,
-    HoldingV1,
-    MetadataV1,
-    TransferInstructionV1,
-} from './token-standard-models-1.0.0/lib/Splice/Api/Token/index.js'
