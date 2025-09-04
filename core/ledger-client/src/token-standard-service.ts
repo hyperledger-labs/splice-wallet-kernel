b0814ea6f81653b00665af7f86cede76b53820e9
Alex Matson <alex.matson@digitalasset.com>
2025-09-04 06:48:12 -0400

    chore: add license headers to source files (#371)

--- core/ledger-client/src/token-standard-service.ts
+++ core/ledger-client/src/token-standard-service.ts
@@ -1,3 +1,6 @@
+// Copyright (c) 2025 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 import { TokenStandardClient } from '@canton-network/core-token-standard'
 import { Logger } from '@canton-network/core-types'
 import { LedgerClient } from './ledger-client.js'
