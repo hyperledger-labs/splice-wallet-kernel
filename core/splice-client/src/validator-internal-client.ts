abf427dca2aed7ff20543381881c221d9361b540
rukmini-basu-da <rukmini.basu@digitalasset.com>
2025-09-09 09:19:21 +0200

    eslint --fix
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    # Conflicts:
    #	core/token-standard/package.json
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>

--- core/splice-client/src/validator-internal-client.ts
+++ core/splice-client/src/validator-internal-client.ts
@@ -1,3 +1,6 @@
+// Copyright (c) 2025 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 import { paths } from './generated-clients/validator-internal'
 import createClient, { Client } from 'openapi-fetch'
 import { Logger } from '@canton-network/core-types'
