e40863ae8eb5cf6f8f935a9279a9ebc53ca01655
Alex Matson <alex.matson@digitalasset.com>
2025-09-10 10:22:01 +0200

    externals
    Signed-off-by: Alex Matson <alex.matson@digitalasset.com>

--- core/token-standard/rollup.config.js
+++ core/token-standard/rollup.config.js
@@ -2,8 +2,21 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import typescript from '@rollup/plugin-typescript'
-import { nodeResolve } from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+
+import fs from 'fs'
+import path from 'path'
+
+const pkgPath = path.resolve(process.cwd(), 'package.json')
+const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf8'))
+
+// Collect deps + peerDeps (but not devDeps, or excepeted ones)
+const exceptions = ['@daml/types']
+const external = [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
+].filter((dep) => !exceptions.includes(dep))
 
 export default {
     input: 'src/index.ts',
@@ -11,15 +24,7 @@ export default {
         dir: 'dist',
         format: 'es',
     },
-    external: [
-        '@canton-network/core-types',
-        '@daml/ledger',
-        '@daml/types',
-        'lodash',
-        'openapi-fetch',
-        'uuid',
-        'zod',
-    ],
+    external,
     plugins: [
         commonjs({
             transformMixedEsModules: true,
