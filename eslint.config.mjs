b27f50c95ec946f5cb96b606f7d69473df242f07
Alex Matson <alex.matson@digitalasset.com>
2025-09-17 15:28:24 -0400

    fix eslint
    Signed-off-by: Alex Matson <alex.matson@digitalasset.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,7 @@
 import js from '@eslint/js'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import { defineConfig } from 'eslint/config'
 import { includeIgnoreFile } from '@eslint/compat'
 import { fileURLToPath } from 'node:url'
 import headers from 'eslint-plugin-headers'
@@ -13,22 +13,24 @@ const gitignorePath = fileURLToPath(new URL('.gitignore', import.meta.url))
 
 export default defineConfig([
     includeIgnoreFile(gitignorePath),
-    globalIgnores([
-        '**/dist',
-        '**/build',
-        '**/_proto',
-        '**/.venv',
-        '**/vite-env.d.ts',
-        '.yarn/**',
-        '.commitlintrc.js',
-        '.pnp.*',
-        'core/wallet-dapp-rpc-client',
-        'core/wallet-user-rpc-client',
-        'core/ledger-client/src/generated-clients',
-        'damljs/**',
-        'docs/wallet-integration-guide/examples/**',
-        'example/playwright-report/**',
-    ]),
+    {
+        ignores: [
+            '**/dist',
+            '**/build',
+            '**/_proto',
+            '**/.venv',
+            '**/vite-env.d.ts',
+            '.yarn/**',
+            '.commitlintrc.js',
+            '.pnp.*',
+            'core/wallet-dapp-rpc-client',
+            'core/wallet-user-rpc-client',
+            'core/ledger-client/src/generated-clients',
+            'damljs/**',
+            'docs/wallet-integration-guide/examples/**',
+            'example/playwright-report/**',
+        ],
+    },
     {
         files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
         languageOptions: { globals: { ...globals.browser, ...globals.node } },
