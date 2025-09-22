12d4eb10dccfae1536f703c0b7f6030b1ea6777e
Marc Juchli <marc.juchli@digitalasset.com>
2025-09-22 13:37:39 +0200

    feat: one step deposit example
    Signed-off-by: Marc Juchli <marc.juchli@digitalasset.com>

---
+++ docs/wallet-integration-guide/examples/scripts/07-one-step-deposit.ts
@@ -0,0 +1,44 @@
+import {
+    WalletSDKImpl,
+    createKeyPair,
+    localNetAuthDefault,
+    localNetLedgerDefault,
+    localNetTokenStandardDefault,
+    localNetTopologyDefault,
+} from '@canton-network/wallet-sdk'
+import { pino } from 'pino'
+import { LOCALNET_VALIDATOR_URL } from '../config.js'
+
+const logger = pino({ name: '07-one-step-deposit', level: 'info' })
+
+const sdk = new WalletSDKImpl().configure({
+    logger,
+    authFactory: localNetAuthDefault,
+    ledgerFactory: localNetLedgerDefault,
+    topologyFactory: localNetTopologyDefault,
+    tokenStandardFactory: localNetTokenStandardDefault,
+})
+
+logger.info('SDK initialized')
+
+await sdk.connect()
+logger.info('Connected to ledger')
+
+await sdk.connectAdmin()
+const exchangeSuffix = Math.floor(Math.random() * 10000)
+const exchangeParty = (
+    await sdk.adminLedger!.allocateInternalParty(`exchangeId_${exchangeSuffix}`)
+).partyDetails!.party
+
+logger.info(`Created exchange party: ${exchangeParty}`)
+
+await sdk.connectTopology(LOCALNET_VALIDATOR_URL)
+const treasuryKeyPair = createKeyPair()
+const treasuryParty = (
+    await sdk.topology?.prepareSignAndSubmitExternalParty(
+        treasuryKeyPair.privateKey,
+        'treasury'
+    )
+)?.partyId
+
+logger.info(`Created treasury party: ${treasuryParty}`)
