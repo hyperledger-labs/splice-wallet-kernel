4488df7c1d14d3dc57d64c1715ca37222c717e50
Pawel Stepien <pawel.stepien@digitalasset.com>
2025-09-22 19:23:50 +0200

    make expire and withdraw separate case. implement cleanup remarks from cr
    Signed-off-by: Pawel Stepien <pawel.stepien@digitalasset.com>

--- docs/wallet-integration-guide/examples/scripts/07-token-standard-reject-expire-withdraw-localnet.ts
+++ docs/wallet-integration-guide/examples/scripts/07-token-standard-reject-expire-withdraw-localnet.ts
@@ -10,7 +10,10 @@ import { pino } from 'pino'
 import { v4 } from 'uuid'
 import { LOCALNET_REGISTRY_API_URL, LOCALNET_VALIDATOR_URL } from '../config.js'
 
-const logger = pino({ name: '04-token-standard-localnet', level: 'info' })
+const logger = pino({
+    name: '07-token-standard-reject-expire-withdraw',
+    level: 'info',
+})
 
 // it is important to configure the SDK correctly else you might run into connectivity or authentication issues
 const sdk = new WalletSDKImpl().configure({
@@ -45,10 +48,6 @@ const receiver = await sdk.topology?.prepareSignAndSubmitExternalParty(
 )
 logger.info(`Created party: ${receiver!.partyId}`)
 
-const synchronizers = await sdk.userLedger?.listSynchronizers()
-
-const synchonizerId = synchronizers!.connectedSynchronizers![0].synchronizerId
-
 await sdk.userLedger
     ?.listWallets()
     .then((wallets) => {
@@ -57,8 +56,6 @@ await sdk.userLedger
     .catch((error) => {
         logger.error({ error }, 'Error listing wallets')
     })
-
-sdk.tokenStandard?.setSynchronizerId(synchonizerId)
 
 sdk.tokenStandard?.setTransferFactoryRegistryUrl(LOCALNET_REGISTRY_API_URL)
 const instrumentAdminPartyId =
@@ -85,11 +82,8 @@ const commandIdTap = await sdk.userLedger?.prepareSignAndExecuteTransaction(
 
 await sdk.userLedger?.waitForCompletion(offsetLatestTap, 5000, commandIdTap!)
 
-const utxos = await sdk.tokenStandard?.listHoldingUtxos(false)
-logger.info(utxos, 'List Available Token Standard Holding UTXOs')
-
 // Alice creates transfer to Bob
-logger.info('Creating transfer transaction')
+logger.info('Creating transfer transaction (reject)')
 const [transferCommandToReject, disclosedContracts2] =
     await sdk.tokenStandard!.createTransfer(
         sender!.partyId,
@@ -99,7 +93,7 @@ const [transferCommandToReject, disclosedContracts2] =
             instrumentId: 'Amulet',
             instrumentAdmin: instrumentAdminPartyId,
         },
-        utxos?.map((t) => t.contractId),
+        undefined,
         'memo-ref'
     )
 
@@ -112,7 +106,7 @@ const transferCommandId =
         v4(),
         disclosedContracts2
     )
-logger.info('Submitted transfer transaction')
+logger.info('Submitted transfer transaction (reject)')
 
 await sdk.userLedger?.waitForCompletion(offsetLatest, 5000, transferCommandId!)
 
@@ -130,7 +124,7 @@ const [rejectTransferCommand, disclosedContracts3] =
         'Reject'
     )
 
-await sdk.userLedger?.prepareSignAndExecuteTransaction(
+const rejectCommandId = await sdk.userLedger?.prepareSignAndExecuteTransaction(
     [{ ExerciseCommand: rejectTransferCommand }],
     keyPairReceiver.privateKey,
     v4(),
@@ -139,14 +133,21 @@ await sdk.userLedger?.prepareSignAndExecuteTransaction(
 
 logger.info('Rejected transfer instruction')
 
+// TODO await completion
+await sdk.userLedger?.waitForCompletion(
+    (await sdk.userLedger?.ledgerEnd())?.offset ?? 0,
+    5000,
+    rejectCommandId!
+)
+
 // Alice creates transfer to Bob with expiry date
 await sdk.setPartyId(sender!.partyId)
-
-const utxos2 = await sdk.tokenStandard?.listHoldingUtxos(false)
-logger.info(utxos2, 'List Available Token Standard Holding UTXOs')
+const utxosAfterRejected = await sdk.tokenStandard?.listHoldingUtxos(false)
 
 const EXPIRATION_MS = 10_000
-const expiryDate = new Date(Date.now() + EXPIRATION_MS) // 5s from now
+const expiryDate = new Date(Date.now() + EXPIRATION_MS)
+
+logger.info('Creating transfer transaction (expire)')
 const [transferCommandToExpire, disclosedContracts4] =
     await sdk.tokenStandard!.createTransfer(
         sender!.partyId,
@@ -156,7 +157,7 @@ const [transferCommandToExpire, disclosedContracts4] =
             instrumentId: 'Amulet',
             instrumentAdmin: instrumentAdminPartyId,
         },
-        utxos2?.map((t) => t.contractId),
+        undefined,
         'memo-ref',
         expiryDate
     )
@@ -170,7 +171,7 @@ const transferCommandId2 =
         v4(),
         disclosedContracts4
     )
-logger.info('Submitted transfer transaction')
+logger.info('Submitted transfer transaction (expire)')
 
 await sdk.userLedger?.waitForCompletion(
     offsetLatest2,
@@ -181,33 +182,24 @@ await sdk.userLedger?.waitForCompletion(
 const pendingInstructions2 =
     await sdk.tokenStandard?.fetchPendingTransferInstructionView()
 
-const transferCid2 = pendingInstructions2?.[0].contractId!
+const expiredTransferCid = pendingInstructions2?.[0].contractId!
 
 // Wait for transfer instruction to expire
 await new Promise((res) => setTimeout(res, EXPIRATION_MS + 5_000))
 
-// Alice withdraws the expired transfer
-const [withdrawTransferCommand, disclosedContracts5] =
-    await sdk.tokenStandard!.exerciseTransferInstructionChoice(
-        transferCid2,
-        'Withdraw'
-    )
-
-await sdk.userLedger?.prepareSignAndExecuteTransaction(
-    [{ ExerciseCommand: withdrawTransferCommand }],
-    keyPairSender.privateKey,
-    v4(),
-    disclosedContracts5
+const utxosAfterExpired = await sdk.tokenStandard?.listHoldingUtxos(false)
+const unlockedUtxo = utxosAfterExpired?.find((t) => t.interfaceViewValue.lock)
+if (!unlockedUtxo) {
+    throw new Error('Unexpected lack of unlocked UTXO after expiration')
+}
+logger.info(
+    { unlockedUtxo },
+    'UTXO used as inputHolding in expired transfer got unlocked after expiration'
 )
 
-logger.info('Withdrawn transfer instruction')
-
-await new Promise((res) => setTimeout(res, 5000))
-
-// Alice creates transfer to Bob
-const utxos3 = await sdk.tokenStandard?.listHoldingUtxos(false)
-logger.info(utxos3, 'List Available Token Standard Holding UTXOs')
-const [transferCommandToAccept, disclosedContracts6] =
+// Alice creates transfer that will be withdrawn
+logger.info('Creating transfer transaction (withdraw)')
+const [transferCommandToWithdraw, disclosedContracts5] =
     await sdk.tokenStandard!.createTransfer(
         sender!.partyId,
         receiver!.partyId,
@@ -216,7 +208,7 @@ const [transferCommandToAccept, disclosedContracts6] =
             instrumentId: 'Amulet',
             instrumentAdmin: instrumentAdminPartyId,
         },
-        utxos3?.map((t) => t.contractId),
+        undefined,
         'memo-ref'
     )
 
@@ -224,12 +216,12 @@ const offsetLatest3 = (await sdk.userLedger?.ledgerEnd())?.offset ?? 0
 
 const transferCommandId3 =
     await sdk.userLedger?.prepareSignAndExecuteTransaction(
-        [{ ExerciseCommand: transferCommandToAccept }],
+        [{ ExerciseCommand: transferCommandToWithdraw }],
         keyPairSender.privateKey,
         v4(),
-        disclosedContracts6
+        disclosedContracts5
     )
-logger.info('Submitted transfer transaction')
+logger.info('Submitted transfer transaction (withdraw)')
 
 await sdk.userLedger?.waitForCompletion(
     offsetLatest3,
@@ -237,43 +229,97 @@ await sdk.userLedger?.waitForCompletion(
     transferCommandId3!
 )
 
-// Bob accepts the transfer
-await sdk.setPartyId(receiver!.partyId)
-
 const pendingInstructions3 =
     await sdk.tokenStandard?.fetchPendingTransferInstructionView()
-const transferCid3 = pendingInstructions3?.[0].contractId!
 
-const [acceptTransferCommand, disclosedContracts7] =
+// Note that previous transfer instruction that expired is still pending until rejected or withdrawn,
+// but input holdings used in expired transfer are unlocked and can be used
+const transferCid3 = pendingInstructions3?.find(
+    (transferInstruction) =>
+        transferInstruction.contractId !== expiredTransferCid
+)!.contractId
+// Alice withdraws the transfer
+const [withdrawTransferCommand, disclosedContracts6] =
     await sdk.tokenStandard!.exerciseTransferInstructionChoice(
-        transferCid3,
-        'Accept'
+        transferCid3!,
+        'Withdraw'
     )
 
 const offsetLatest4 = (await sdk.userLedger?.ledgerEnd())?.offset ?? 0
+const withdrawCommandId =
+    await sdk.userLedger?.prepareSignAndExecuteTransaction(
+        [{ ExerciseCommand: withdrawTransferCommand }],
+        keyPairSender.privateKey,
+        v4(),
+        disclosedContracts6
+    )
+
+logger.info('Withdrawn transfer instruction')
+await sdk.userLedger?.waitForCompletion(offsetLatest4, 5000, withdrawCommandId!)
+
+const utxosAfterWithdraw = await sdk.tokenStandard?.listHoldingUtxos(false)
+
+// Alice creates transfer to Bob to accept using reclaimed holdings
+const [transferCommandToAccept, disclosedContracts7] =
+    await sdk.tokenStandard!.createTransfer(
+        sender!.partyId,
+        receiver!.partyId,
+        '100',
+        {
+            instrumentId: 'Amulet',
+            instrumentAdmin: instrumentAdminPartyId,
+        },
+        undefined,
+        'memo-ref'
+    )
+
+const offsetLatest5 = (await sdk.userLedger?.ledgerEnd())?.offset ?? 0
 
 const transferCommandId4 =
     await sdk.userLedger?.prepareSignAndExecuteTransaction(
-        [{ ExerciseCommand: acceptTransferCommand }],
-        keyPairReceiver.privateKey,
+        [{ ExerciseCommand: transferCommandToAccept }],
+        keyPairSender.privateKey,
         v4(),
         disclosedContracts7
     )
-logger.info('Accepted transfer instruction')
+logger.info('Submitted transfer transaction')
 
 await sdk.userLedger?.waitForCompletion(
-    offsetLatest4,
+    offsetLatest5,
     5000,
     transferCommandId4!
 )
 
-await new Promise((res) => setTimeout(res, 5000))
-{
-    await sdk.setPartyId(sender!.partyId)
-    const aliceHoldings = await sdk.tokenStandard?.listHoldingTransactions()
-    logger.info(aliceHoldings, '[ALICE] holding transactions')
+// Bob accepts the transfer
+await sdk.setPartyId(receiver!.partyId)
 
-    await sdk.setPartyId(receiver!.partyId)
-    const bobHoldings = await sdk.tokenStandard?.listHoldingTransactions()
-    logger.info(bobHoldings, '[BOB] holding transactions')
-}
+const pendingInstructions4 =
+    await sdk.tokenStandard?.fetchPendingTransferInstructionView()
+
+const transferCid4 = pendingInstructions4?.find(
+    (transferInstruction) =>
+        transferInstruction.contractId !== expiredTransferCid
+)!.contractId
+
+const [acceptTransferCommand, disclosedContracts8] =
+    await sdk.tokenStandard!.exerciseTransferInstructionChoice(
+        transferCid4!,
+        'Accept'
+    )
+
+const offsetLatest6 = (await sdk.userLedger?.ledgerEnd())?.offset ?? 0
+
+const transferCommandId5 =
+    await sdk.userLedger?.prepareSignAndExecuteTransaction(
+        [{ ExerciseCommand: acceptTransferCommand }],
+        keyPairReceiver.privateKey,
+        v4(),
+        disclosedContracts8
+    )
+logger.info('Accepted transfer instruction')
+
+await sdk.userLedger?.waitForCompletion(
+    offsetLatest6,
+    5000,
+    transferCommandId5!
+)
