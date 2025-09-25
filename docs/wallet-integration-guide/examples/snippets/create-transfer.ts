e253478c634c48f4dd70d6299f960d1f02cc4a76
phillip olesen <phillip.olesen@digitalasset.com>
2025-09-25 15:56:33 +0200

    last fix i swear
    Signed-off-by: phillip olesen <phillip.olesen@digitalasset.com>

--- docs/wallet-integration-guide/examples/snippets/create-transfer.ts
+++ docs/wallet-integration-guide/examples/snippets/create-transfer.ts
@@ -6,47 +6,50 @@ import {
 } from '@canton-network/wallet-sdk'
 import { v4 } from 'uuid'
 
-const sdk = new WalletSDKImpl().configure({
-    logger: console,
-    authFactory: localNetAuthDefault,
-    ledgerFactory: localNetLedgerDefault,
-    tokenStandardFactory: localNetTokenStandardDefault,
-})
+// @disable-snapshot-test
+export default async function () {
+    const sdk = new WalletSDKImpl().configure({
+        logger: console,
+        authFactory: localNetAuthDefault,
+        ledgerFactory: localNetLedgerDefault,
+        tokenStandardFactory: localNetTokenStandardDefault,
+    })
 
-const sender = 'sender-party'
-const senderKey = 'private-key-for-my-party'
-const instrumentAdminPartyId = 'admin-of-the-instrument'
+    const sender = 'sender-party'
+    const senderKey = 'private-key-for-my-party'
+    const instrumentAdminPartyId = 'admin-of-the-instrument'
 
-const receiver = 'receiver-party'
+    const receiver = 'receiver-party'
 
-await sdk.connect()
-await sdk.setPartyId(sender)
+    await sdk.connect()
+    await sdk.setPartyId(sender)
 
-const [transferCommand, disclosedContracts2] =
-    await sdk.tokenStandard!.createTransfer(
-        sender,
-        receiver,
-        '100',
-        {
-            instrumentId: 'Amulet',
-            instrumentAdmin: instrumentAdminPartyId,
-        },
-        [],
-        'memo-ref'
+    const [transferCommand, disclosedContracts2] =
+        await sdk.tokenStandard!.createTransfer(
+            sender,
+            receiver,
+            '100',
+            {
+                instrumentId: 'Amulet',
+                instrumentAdmin: instrumentAdminPartyId,
+            },
+            [],
+            'memo-ref'
+        )
+
+    const offsetLatest = (await sdk.userLedger?.ledgerEnd())?.offset ?? 0
+
+    const transferCommandId =
+        await sdk.userLedger?.prepareSignAndExecuteTransaction(
+            [{ ExerciseCommand: transferCommand }],
+            senderKey,
+            v4(),
+            disclosedContracts2
+        )
+
+    const completion = await sdk.userLedger?.waitForCompletion(
+        offsetLatest,
+        5000,
+        transferCommandId!
     )
-
-const offsetLatest = (await sdk.userLedger?.ledgerEnd())?.offset ?? 0
-
-const transferCommandId =
-    await sdk.userLedger?.prepareSignAndExecuteTransaction(
-        [{ ExerciseCommand: transferCommand }],
-        senderKey,
-        v4(),
-        disclosedContracts2
-    )
-
-const completion = await sdk.userLedger?.waitForCompletion(
-    offsetLatest,
-    5000,
-    transferCommandId!
-)
+}
