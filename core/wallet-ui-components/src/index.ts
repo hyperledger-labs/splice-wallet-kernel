16d72a6eaeb90aada7a45172013ad23db3d97562
Alina Radyukevich <a.radyukevich@pixelplex.io>
2025-10-13 18:07:50 +0300

    feat: implement 404 page
    Signed-off-by: Alina Radyukevich <a.radyukevich@pixelplex.io>

--- core/wallet-ui-components/src/index.ts
+++ core/wallet-ui-components/src/index.ts
@@ -6,6 +6,7 @@ export * from './components/AppLayout.js'
 export * from './components/Discovery.js'
 export * from './components/NetworkTable.js'
 export * from './components/NetworkForm.js'
+export * from './components/NotFound.js'
 
 export * from './windows/discovery.js'
 export * from './windows/popup.js'
