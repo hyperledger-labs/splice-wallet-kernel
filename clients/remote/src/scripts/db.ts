0af55b23658b32cbb67aa908295c4498903965c7
Alex Matson <alex.matson@digitalasset.com>
2025-10-16 15:00:36 -0400

    feat(clients-remote): setup CLI support (#705)
    * feat(clients-remote): setup CLI support
    
    Signed-off-by: Alex Matson <alex.matson@digitalasset.com>
    
    * add shebang
    
    Signed-off-by: Alex Matson <alex.matson@digitalasset.com>
    
    * fix config
    
    Signed-off-by: Alex Matson <alex.matson@digitalasset.com>
    
    * feat(clients-remote): setup CLI entrypoint
    
    Signed-off-by: Alex Matson <alex.matson@digitalasset.com>
    
    ---------
    
    Signed-off-by: Alex Matson <alex.matson@digitalasset.com>

--- clients/remote/src/scripts/db.ts
+++ clients/remote/src/scripts/db.ts
@@ -3,10 +3,8 @@
 
 import { createCLI } from '@canton-network/core-wallet-store-sql'
 import { ConfigUtils } from '../config/ConfigUtils.js'
-import { configSchema } from '../config/Config.js'
 
 const configPath = process.env.NETWORK_CONFIG_PATH || '../test/config.json'
-const configFile = ConfigUtils.loadConfigFile(configPath)
-const config = configSchema.parse(configFile)
+const config = ConfigUtils.loadConfigFile(configPath)
 const cli = createCLI(config.store)
 cli.parseAsync(process.argv)
