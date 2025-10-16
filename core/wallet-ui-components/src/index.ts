e35f73389f1dd2aa9c7722b338b6f846adedc6bd
PixelPlex Dev team <10460630+pixelplex@users.noreply.github.com>
2025-10-16 12:39:35 +0300

    feat: implement 404 page (#695)
    Signed-off-by: Alina Radyukevich <a.radyukevich@pixelplex.io>

--- core/wallet-ui-components/src/index.ts
+++ core/wallet-ui-components/src/index.ts
@@ -7,6 +7,7 @@ export * from './components/CustomToast.js'
 export * from './components/Discovery.js'
 export * from './components/NetworkTable.js'
 export * from './components/NetworkForm.js'
+export * from './components/NotFound.js'
 
 export * from './windows/discovery.js'
 export * from './windows/popup.js'
