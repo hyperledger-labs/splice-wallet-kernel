d19482b9598adde6d03d8f54a3cf8762180680cd
phillip olesen <phillip.olesen@digitalasset.com>
2025-10-17 13:53:12 +0800

    MORE
    Signed-off-by: phillip olesen <phillip.olesen@digitalasset.com>

--- docs/wallet-integration-guide/examples/scripts/stress/01-party-stress-test.ts
+++ docs/wallet-integration-guide/examples/scripts/stress/01-party-stress-test.ts
@@ -12,6 +12,7 @@ import { pino } from 'pino'
 import { v4 } from 'uuid'
 
 const logger = pino({ name: '01-party-stress', level: 'info' })
+const warnOnly = pino({ name: '01-party-stress', level: 'warn' })
 
 // it is important to configure the SDK correctly else you might run into connectivity or authentication issues
 const sdk = new WalletSDKImpl().configure({
@@ -24,6 +25,9 @@ const sdk = new WalletSDKImpl().configure({
 await sdk.connect()
 await sdk.connectAdmin()
 await sdk.connectTopology(localNetStaticConfig.LOCALNET_SCAN_PROXY_API_URL)
+sdk.tokenStandard?.setTransferFactoryRegistryUrl(
+    localNetStaticConfig.LOCALNET_REGISTRY_API_URL
+)
 
 type partyDefiniton = {
     keyPair: { publicKey: string; privateKey: string }
@@ -38,89 +42,56 @@ const instrument = {
     instrumentAdmin: (await sdk.tokenStandard?.getInstrumentAdmin()) || '',
 }
 
-async function allocatePartiesParallel(count: number, iterationAmount: number) {
-    const results = new Array(count)
-    const promises = []
-    let completed = 0
-    let lastCheck = 0
-    for (let i = 1; i <= count; i++) {
-        promises.push(
-            (async () => {
-                try {
-                    const keyPair = createKeyPair()
-                    const allocatedParty =
-                        await sdk.userLedger?.signAndAllocateExternalParty(
-                            keyPair.privateKey
-                        )
-                    results[i] = { success: true, value: allocatedParty }
-                    createdParties.push({
-                        keyPair,
-                        partyId: allocatedParty!.partyId!,
-                    })
-                    completed++
-                } catch (error) {
-                    results[i] = { success: false, error }
-                    completed++
-                }
-            })()
-        )
-        if (i % iterationAmount == 0 && i > 0) {
-            logger.info(
-                `created ${iterationAmount} more party promises, ${i} out of ${count}`
-            )
+const validatorOperatorParty = (await sdk.validator?.getValidatorUser()) || ''
 
-            // Wait until 'completed' is a multiple of iterationAmount
-            await new Promise((resolve) => {
-                const check = () => {
-                    const currentCheck = completed
-                    if (
-                        currentCheck % iterationAmount == 0 &&
-                        currentCheck != lastCheck
-                    ) {
-                        lastCheck = currentCheck
-                        logger.info(
-                            `${currentCheck} out of ${count} party allocations have finished`
-                        )
-                        resolve(undefined)
-                    } else {
-                        setTimeout(check, 100)
-                    }
-                }
-                check()
-            })
-        }
+async function allocateParty() {
+    const keyPair = createKeyPair()
+    const allocatedParty = await sdk.userLedger?.signAndAllocateExternalParty(
+        keyPair.privateKey
+    )
+    const definition = {
+        keyPair,
+        partyId: allocatedParty!.partyId,
     }
-    await Promise.allSettled(promises)
-    return results
-}
 
-function getRandomInt(min: number, max: number): number {
-    return Math.floor(Math.random() * (max - min)) + min
+    createdParties.push(definition)
+
+    return definition
+}
+function getRandomParty(): partyDefiniton {
+    const rando = Math.floor(Math.random() * createdParties.length)
+    return createdParties[rando]
 }
 
 async function tapAndTransfer(fromParty: partyDefiniton, count: number) {
     const transferSdk = new WalletSDKImpl().configure({
-        logger: logger,
+        logger: warnOnly,
         authFactory: localNetAuthDefault,
         ledgerFactory: localNetLedgerDefault,
         topologyFactory: localNetTopologyDefault,
         tokenStandardFactory: localNetTokenStandardDefault,
     })
-    await sdk.connect()
-    await sdk.connectAdmin()
-    await sdk.connectTopology(localNetStaticConfig.LOCALNET_SCAN_PROXY_API_URL)
+    await transferSdk.connect()
+    await transferSdk.connectAdmin()
+    await transferSdk.connectTopology(
+        localNetStaticConfig.LOCALNET_SCAN_PROXY_API_URL
+    )
+    transferSdk.tokenStandard?.setTransferFactoryRegistryUrl(
+        localNetStaticConfig.LOCALNET_REGISTRY_API_URL
+    )
+    await transferSdk.setPartyId(fromParty.partyId)
 
     for (let i = 1; i <= count; i++) {
         try {
             const [tapCommand, disclosedContracts] =
-                await sdk.tokenStandard!.createTap(
+                await transferSdk.tokenStandard!.createTap(
                     fromParty.partyId,
                     '100',
                     instrument
                 )
 
             //we create a tap and then perform a transfer after it is complete
-            sdk.userLedger
+            transferSdk.userLedger
                 ?.prepareSignExecuteAndWaitFor(
                     tapCommand,
                     fromParty.keyPair.privateKey,
@@ -128,10 +99,9 @@ async function tapAndTransfer(fromParty: partyDefiniton, count: number) {
                     disclosedContracts
                 )
                 .then(async () => {
-                    const rando = getRandomInt(0, createdParties.length)
-                    const toParty = createdParties[rando]
+                    const toParty = getRandomParty()
                     const [command, dc] =
-                        await sdk.tokenStandard!.createTransfer(
+                        await transferSdk.tokenStandard!.createTransfer(
                             fromParty.partyId,
                             toParty.partyId,
                             '100',
@@ -140,11 +110,16 @@ async function tapAndTransfer(fromParty: partyDefiniton, count: number) {
 
                     const id = v4()
                     allTransferCommandIds.push(id)
-                    await sdk.userLedger?.prepareSignAndExecuteTransaction(
+                    await transferSdk.userLedger?.prepareSignAndExecuteTransaction(
                         command,
                         fromParty.keyPair.privateKey,
                         id,
                         dc
+                    )
+                })
+                .catch((error) => {
+                    logger.error(
+                        `[${fromParty.partyId}] Tap ${i} failed: ${error}`
                     )
                 })
         } catch (error) {
@@ -153,32 +128,59 @@ async function tapAndTransfer(fromParty: partyDefiniton, count: number) {
             )
         }
     }
+    logger.info(`[${fromParty.partyId}] completed ${count} transfers`)
 }
 
-//at approximately 50 we start seeing problem if we don't use expectHeavyLoad on allocation
-const parallelCreationCount = 200
-const iterationAmount = 50
-logger.info(
-    `starting simulation stress test for ${parallelCreationCount} user party creations`
-)
-const allocationResults = await allocatePartiesParallel(
-    parallelCreationCount,
-    iterationAmount
-)
-const successCount = allocationResults.filter((r) => r?.success).length
-allocationResults.forEach((result, index) => {
-    if (result) {
-        if (result.success) {
-            logger.info(
-                `Party ${index} allocated: ${JSON.stringify(result.value)}`
-            )
-        } else {
-            logger.error(`Party ${index} allocation failed: ${result.error}`)
-        }
-    } else {
-        logger.error(`Party ${index} allocation resulted in undefined`)
+logger.info('starting stress run')
+
+//ensure the validator have enough funds
+await sdk.setPartyId(validatorOperatorParty)
+
+await sdk.tokenStandard?.createAndSubmitTapInternal(
+    validatorOperatorParty,
+    '2000000000',
+    {
+        instrumentId: 'Amulet',
+        instrumentAdmin: instrument.instrumentAdmin,
     }
-})
-logger.info(
-    `Requested ${parallelCreationCount}, Successfully allocated ${successCount}`
 )
+
+const partiesPerInterval = process.env.PARTIES_PER_INTERVAL
+    ? parseInt(process.env.PARTIES_PER_INTERVAL, 0)
+    : 5
+const intervalLengthMs = process.env.INTERVAL_LENGTH_MS
+    ? parseInt(process.env.INTERVAL_LENGTH_MS, 0)
+    : 20000
+const transfersPerParty = process.env.TRANSFERS_PER_PARTY
+    ? parseInt(process.env.TRANSFERS_PER_PARTY, 0)
+    : 10
+
+let currentInterval = 0
+
+setInterval(async () => {
+    try {
+        currentInterval++
+        logger.info(
+            `[${currentInterval}] Starting allocation of ${partiesPerInterval} parties and ${transfersPerParty} transfers each`
+        )
+        for (let i = 1; i <= partiesPerInterval; i++) {
+            allocateParty()
+                .then(async (party) => {
+                    await tapAndTransfer(party, transfersPerParty)
+                })
+                .catch((error) => {
+                    logger.error(`Error allocating party: ${error}`)
+                })
+            await new Promise((resolve) =>
+                setTimeout(resolve, partiesPerInterval / intervalLengthMs)
+            )
+        }
+    } catch (error) {
+        logger.error(`Error in interval ${currentInterval}: ${error}`)
+    }
+}, intervalLengthMs)
+
+process.on('SIGINT', () => {
+    logger.info('Caught interrupt signal, exiting...')
+    process.exit()
+})
