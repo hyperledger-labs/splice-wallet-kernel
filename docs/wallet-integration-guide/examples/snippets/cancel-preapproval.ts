71a082f35077cc6723f547cbd6e63b14ea763adf
phillip olesen <phillip.olesen@digitalasset.com>
2025-11-05 12:46:53 +0100

    docs: add sections for preapproval exercies and decoding topology tx
    Signed-off-by: phillip olesen <phillip.olesen@digitalasset.com>

---
+++ docs/wallet-integration-guide/examples/snippets/cancel-preapproval.ts
@@ -0,0 +1,39 @@
+import {
+    WalletSDKImpl,
+    localNetAuthDefault,
+    localNetLedgerDefault,
+    localNetTokenStandardDefault,
+    localValidatorDefault,
+} from '@canton-network/wallet-sdk'
+import { v4 } from 'uuid'
+
+// @disable-snapshot-test
+export default async function () {
+    const sdk = new WalletSDKImpl().configure({
+        logger: console,
+        authFactory: localNetAuthDefault,
+        ledgerFactory: localNetLedgerDefault,
+        tokenStandardFactory: localNetTokenStandardDefault,
+        validatorFactory: localValidatorDefault,
+    })
+
+    const myParty = 'my-party'
+
+    await sdk.connect()
+    await sdk.setPartyId(myParty)
+    const validatorOperatorParty = await sdk.validator?.getValidatorUser()
+
+    const preapproval =
+        await sdk.tokenStandard!.waitForPreapprovalFromScanProxy(
+            myParty,
+            'Amulet'
+        )
+    const [renewCmd, disclosedContractsRenew] =
+        await sdk.tokenStandard!.createCancelTransferPreapproval(
+            preapproval.contractId,
+            preapproval.templateId,
+            validatorOperatorParty!
+        )
+
+    //Sign and execute the above command
+}
---
+++ docs/wallet-integration-guide/examples/snippets/decode-topology-tx.ts
@@ -0,0 +1,34 @@
+import {
+    localNetAuthDefault,
+    localNetLedgerDefault,
+    localNetStaticConfig,
+    localNetTopologyDefault,
+    TopologyController,
+    WalletSDKImpl,
+} from '@canton-network/wallet-sdk'
+
+// @disable-snapshot-test
+export default async function () {
+    // it is important to configure the SDK correctly else you might run into connectivity or authentication issues
+    const sdk = new WalletSDKImpl().configure({
+        logger: console,
+        authFactory: localNetAuthDefault, // or use your specific configuration
+        ledgerFactory: localNetLedgerDefault, // or use your specific configuration
+        topologyFactory: localNetTopologyDefault, // or use your specific configuration
+    })
+
+    await sdk.connectTopology(localNetStaticConfig.LOCALNET_SCAN_PROXY_API_URL)
+
+    const { publicKey, privateKey } = TopologyController.createNewKeyPair()
+    //partyHint is optional but recommended to make it easier to identify the party
+    const partyHint = 'my-wallet-1'
+
+    const generateExternalPartyResponse =
+        await sdk.userLedger?.generateExternalParty(privateKey, partyHint)
+
+    return generateExternalPartyResponse!.topologyTransactions!.map(
+        (topologyTx) => {
+            sdk.userLedger!.toDecodedTopologyTransaction(topologyTx)
+        }
+    )
+}
