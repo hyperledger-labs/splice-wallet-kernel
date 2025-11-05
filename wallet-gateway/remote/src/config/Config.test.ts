6ba88f6b76b1b83da3eacfac0a36924d1dfa3cd6
Alex Matson <alex.matson@digitalasset.com>
2025-11-05 11:11:41 -0500

    cleanup
    Signed-off-by: Alex Matson <alex.matson@digitalasset.com>

--- wallet-gateway/remote/src/config/Config.test.ts
+++ wallet-gateway/remote/src/config/Config.test.ts
@@ -22,28 +22,3 @@ test('config from json file', async () => {
         )
     }
 })
-
-/**
- *                 "id": "canton:local-oauth",
-                "name": "Local (OAuth IDP)",
-                "description": "Mock OAuth IDP",
-                "synchronizerId": "wallet::1220e7b23ea52eb5c672fb0b1cdbc916922ffed3dd7676c223a605664315e2d43edd",
-                "identityProviderId": "idp-mock-oauth",
-                "auth": {
-                    "method": "authorization_code",
-                    "clientId": "operator",
-                    "scope": "operator",
-                    "audience": "https://daml.com/jwt/aud/participant/participant1::1220d44fc1c3ba0b5bdf7b956ee71bc94ebe2d23258dc268fdf0824fbaeff2c61424"
-                },
-                "adminAuth": {
-                    "method": "client_credentials",
-                    "scope": "daml_ledger_api",
-                    "audience": "https://daml.com/jwt/aud/participant/participant1::1220d44fc1c3ba0b5bdf7b956ee71bc94ebe2d23258dc268fdf0824fbaeff2c61424",
-                    "clientId": "participant_admin",
-                    "clientSecret": "admin-client-secret"
-                },
-                "ledgerApi": {
-                    "baseUrl": "http://127.0.0.1:5003"
-                }
-
- */
