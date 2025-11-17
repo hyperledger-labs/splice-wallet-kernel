fa8d6ce77a8248a4c516fede3648045ec1bdcc85
Pawel Stepien <pawel.stepien@digitalasset.com>
2025-11-17 15:28:15 +0100

    aggregate wallet-sdk e2e for main and dev nets under one step
    Signed-off-by: Pawel Stepien <pawel.stepien@digitalasset.com>

--- sdk/wallet-sdk/src/index.ts
+++ sdk/wallet-sdk/src/index.ts
@@ -7,7 +7,7 @@ import {
     localNetTokenStandardDefault,
     TokenStandardController,
 } from './tokenStandardController.js'
-import { ScanClient, ScanProxyClient } from '@canton-network/core-splice-client'
+import { ScanProxyClient } from '@canton-network/core-splice-client'
 import {
     localNetTopologyDefault,
     TopologyController,
@@ -248,14 +248,6 @@ export class WalletSDKImpl implements WalletSDK {
             const amuletSynchronizerId =
                 await scanProxyClient.getAmuletSynchronizerId()
 
-            // TODO sanity check for CI. Remove before merging.
-            const scanClient = new ScanClient(
-                synchronizer.href,
-                this.logger!,
-                accessToken
-            )
-            const version = await scanClient.get('/version')
-            this.logger!.info({ version }, 'Splice version')
             if (amuletSynchronizerId === undefined) {
                 throw new Error(
                     'SynchronizerId is not defined in ScanProxyClient.'
