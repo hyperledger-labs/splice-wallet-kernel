7ca45c703730d1b2e4a4de1afba877b1d5c5fcf0
Pawel Stepien <pawel.stepien@digitalasset.com>
2025-11-18 11:11:54 +0100

    handle pr comments and adjust SpliceMessageEvent to satisfy TS
    Signed-off-by: Pawel Stepien <pawel.stepien@digitalasset.com>

--- docs/wallet-integration-guide/examples/snippets/read-active-contracts.ts
+++ docs/wallet-integration-guide/examples/snippets/read-active-contracts.ts
@@ -4,19 +4,6 @@ import {
     localNetLedgerDefault,
 } from '@canton-network/wallet-sdk'
 
-const PING_NEW = '#canton-builtin-admin-workflow-ping:Canton.Internal.Ping:Ping' // 3.4+
-const PING_OLD = '#AdminWorkflows:Canton.Internal.Ping:Ping' // 3.3
-
-function parseMajorMinor(version: string): [number, number] {
-    const [major, minor] = version.split('.')
-    return [Number(major) || 0, Number(minor) || 0]
-}
-function greaterOrEqualVersion(a: string, b: string): boolean {
-    const [aMajor, aMinor] = parseMajorMinor(a)
-    const [bMajor, bMinor] = parseMajorMinor(b)
-    return aMajor > bMajor || (aMajor === bMajor && aMinor >= bMinor)
-}
-
 export default async function () {
     const sdk = new WalletSDKImpl().configure({
         logger: console,
@@ -28,9 +15,7 @@ export default async function () {
     const myParty = global.EXISTING_PARTY_1
     const offset = (await sdk.userLedger!.ledgerEnd()!).offset
     //we use holdings as an example here
-    const version = sdk.userLedger!.getCurrentClientVersion()
-    const isNewTemplate = greaterOrEqualVersion(version, '3.4')
-    const myTemplateId = isNewTemplate ? PING_NEW : PING_OLD
+    const myTemplateId = '#splice-amulet:Splice.Amulet:Amulet'
 
     await sdk.userLedger!.activeContracts({
         offset,
