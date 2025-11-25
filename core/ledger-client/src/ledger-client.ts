53db2a4ac31fd3692527255c560f2acbb4e610a0
Pawel Stepien <pawel.stepien@digitalasset.com>
2025-11-25 13:11:33 +0100

    increase waitForPartyAndGrantUserRights retry interval when expecting heavy load
    Signed-off-by: Pawel Stepien <pawel.stepien@digitalasset.com>

--- core/ledger-client/src/ledger-client.ts
+++ core/ledger-client/src/ledger-client.ts
@@ -307,12 +307,14 @@ export class LedgerClient {
      * @param userId The ID of the user to grant rights to.
      * @param partyId The ID of the party to grant rights for.
      * @param maxTries Optional max number of retries with default 30. May be increased if expecting heavy load.
+     * @param retryIntervalMs Optional interval between retries to verify that party exists with default 2000ms. May be increased if expecting heavy load.
      * @returns A promise that resolves when the rights have been granted.
      */
     public async waitForPartyAndGrantUserRights(
         userId: string,
         partyId: PartyId,
-        maxTries: number = 30
+        maxTries: number = 30,
+        retryIntervalMs: number = 2000
     ) {
         await this.init()
         // Wait for party to appear on participant
@@ -322,12 +324,14 @@ export class LedgerClient {
         while (!partyFound && tries < maxTries) {
             partyFound = await this.checkIfPartyExists(partyId)
 
-            await new Promise((resolve) => setTimeout(resolve, 2000))
+            await new Promise((resolve) => setTimeout(resolve, retryIntervalMs))
             tries++
         }
 
         if (tries >= maxTries) {
-            throw new Error('timed out waiting for new party to appear')
+            throw new Error(
+                `timed out waiting for new party to appear after ${maxTries} tries`
+            )
         }
 
         const result = await this.grantRights(userId, {
