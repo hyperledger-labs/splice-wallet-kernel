8d57d7aaa7e6f78de6868a9842bb25b6a91e2a65
fayi-da <fayimora.femibalogun@digitalasset.com>
2026-01-05 17:14:42 +0000

    feat(example-portfolio): integrate routing in portfolio app
    - Add tanstack router for routing
    - Add tanstack-devtools for easier debugging
    - Move the existing stuff to /old route
    
    Signed-off-by: fayi-da <fayimora.femibalogun@digitalasset.com>

--- examples/portfolio/src/main.tsx
+++ examples/portfolio/src/main.tsx
@@ -1,10 +1,43 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { createRouter, RouterProvider } from '@tanstack/react-router'
+import { routeTree } from './routeTree.gen'
+import ReactDOM from 'react-dom/client'
 
-createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <App />
-    </StrictMode>
-)
+import { RegistryServiceProvider } from './contexts/RegistriesServiceProvider'
+import { ConnectionProvider } from './contexts/ConnectionProvider'
+import { PortfolioProvider } from './contexts/PortfolioProvider'
+
+const router = createRouter({
+    routeTree,
+    context: {},
+    defaultPreload: 'intent',
+    scrollRestoration: true,
+    defaultStructuralSharing: true,
+    defaultPreloadStaleTime: 0,
+})
+
+// Register the router instance for type safety
+declare module '@tanstack/react-router' {
+    interface Register {
+        router: typeof router
+    }
+}
+
+// Render the app
+const rootElement = document.getElementById('app')
+
+if (rootElement && !rootElement.innerHTML) {
+    const root = ReactDOM.createRoot(rootElement)
+    root.render(
+        <StrictMode>
+            <RegistryServiceProvider>
+                <ConnectionProvider>
+                    <PortfolioProvider>
+                        <RouterProvider router={router} />
+                    </PortfolioProvider>
+                </ConnectionProvider>
+            </RegistryServiceProvider>
+        </StrictMode>
+    )
+}
