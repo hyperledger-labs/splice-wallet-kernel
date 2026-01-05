03b746fe4c2b0a72bf8a37720576cdd8411ff9a6
Jasper Van der Jeugt <jasper.vanderjeugt@digitalasset.com>
2026-01-05 11:42:18 +0100

    fix: update copyright headers to 2026 (#1077)
    Happy new year everyone
    
    Signed-off-by: Jasper Van der Jeugt <jasper.vanderjeugt@digitalasset.com>

--- wallet-gateway/remote/src/utils.ts
+++ wallet-gateway/remote/src/utils.ts
@@ -1,6 +1,5 @@
-// Copyright (c) 2025 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// Copyright (c) 2025-2026 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
-// Disabled unused vars rule to allow for future implementations
 
 import { v4 } from 'uuid'
 import { LedgerClient, PostRequest } from '@canton-network/core-ledger-client'
