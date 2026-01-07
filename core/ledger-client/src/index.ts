b886d7dd2c0cefa0cc4de3ce09973c67fdfaae07
rukmini-basu-da <rukmini.basu@digitalasset.com>
2026-01-07 09:24:09 -0500

    add websocket client
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>

--- core/ledger-client/src/index.ts
+++ core/ledger-client/src/index.ts
@@ -42,3 +42,6 @@ export {
     JSContractEntry,
     defaultRetryableOptions,
 } from './ledger-api-utils.js'
+
+export * from './web-socket-client.js'
+export { GetUpdatesRequest } from './generated-clients/asyncapi-3.4.7.js'
