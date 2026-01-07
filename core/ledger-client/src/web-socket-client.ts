63b9816ab07ef799a5eeccb661b25576d9ec163c
rukmini-basu-da <rukmini.basu@digitalasset.com>
2026-01-07 14:02:28 -0500

    working implementation with updates
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>

--- core/ledger-client/src/web-socket-client.ts
+++ core/ledger-client/src/web-socket-client.ts
@@ -4,16 +4,10 @@
 import { PartyId } from '@canton-network/core-types'
 import {
     CHANNELS,
-    GetActiveContractsRequest,
     JsGetUpdatesResponse,
-    JsGetActiveContractsResponse,
-    CompletionStreamResponse,
 } from './generated-clients/asyncapi-3.4.7.js'
 import { Logger } from 'pino'
-import {
-    TransactionFilterBySetup,
-    TransactionFilterBySetup2,
-} from './ledger-api-utils.js'
+import { TransactionFilterBySetup2 } from './ledger-api-utils.js'
 import { AccessTokenProvider } from '@canton-network/core-wallet-auth'
 
 export class WebSocketClient {
@@ -65,148 +59,6 @@ export class WebSocketClient {
         )
     }
 
-    //TODO: return the subscription, not the ActiveContractsREsponse
-    async subscribeToActiveContracts2(
-        interfaceIds: string[],
-        partyId: PartyId,
-        offset?: number
-    ) {
-        await this.init()
-        const wsUpdatesUrl = `${this.baseUrl}${CHANNELS.v2_state_active_contracts}`
-
-        this.logger.info(`active contracts url: ${wsUpdatesUrl}`)
-        const filter = TransactionFilterBySetup(interfaceIds, {
-            partyId,
-        })
-
-        const request = {
-            filter,
-            verbose: false,
-            activeAtOffset: offset ?? 0,
-        }
-
-        return new Promise((resolve, reject) => {
-            const ws = new WebSocket(wsUpdatesUrl, this.protocol)
-            const results: JsGetActiveContractsResponse[] = []
-            let finished = false
-            let error: Error | null = null
-            setTimeout(() => {
-                if (!finished && !error && results.length === 0) {
-                    error = Error(
-                        `No data received from WebSocket ${wsUpdatesUrl} within ${this.wsSupportBackOff}ms`
-                    )
-                    reject(error)
-                    ws.close()
-                }
-            }, this.wsSupportBackOff)
-
-            ws.onopen = () => {
-                setTimeout(() => {
-                    ws.send(JSON.stringify(request))
-                }, this.wsSupportBackOff)
-            }
-
-            ws.onmessage = (event: MessageEvent<string>) => {
-                try {
-                    this.logger.info(`received event: ${event.data}`)
-                    results.push(JSON.parse(event.data))
-                } catch (err) {
-                    this.logger.error(`Invalid JSON ${err}`)
-                }
-            }
-
-            ws.onerror = () => {
-                error = new Error(`WebSocket error`)
-                reject(error)
-            }
-
-            ws.onclose = () => {
-                finished = true
-                if (!error) {
-                    resolve({
-                        updates: results,
-                    })
-                }
-            }
-        })
-    }
-
-    subscribeToActiveContractsStreaming(
-        interfaceIds: string[],
-        templateIds: string[],
-        partyId: string,
-        offset?: number
-    ): AsyncIterableIterator<JsGetActiveContractsResponse> {
-        const messageQueue: JsGetActiveContractsResponse[] = []
-        let resolveNext: (() => void) | null = null
-        let isClosed = false
-        let streamError: Error | null = null
-
-        // We create a wrapper generator
-        const generator = async function* (this: WebSocketClient) {
-            await this.init() // Now init happens inside the generator
-
-            const wsUpdatesUrl = `${this.baseUrl}${CHANNELS.v2_state_active_contracts}`
-            const request = {
-                filter: TransactionFilterBySetup2(interfaceIds, templateIds, {
-                    partyId,
-                }),
-                verbose: false,
-                activeAtOffset: offset ?? 0,
-            }
-
-            this.logger.info(request, `DEBUGGING REQUEST`)
-
-            const ws = new WebSocket(wsUpdatesUrl, this.protocol)
-
-            ws.onopen = () => {
-                this.logger.info(
-                    `OPENING WEBSOCKET CONNECTION AND SENDING REQUEST`
-                )
-                ws.send(JSON.stringify(request))
-            }
-            ws.onmessage = (event) => {
-                messageQueue.push(JSON.parse(event.data as string))
-                this.logger.info(`Received event: ${event.data}`)
-                resolveNext?.()
-            }
-            ws.onerror = () => {
-                streamError = new Error('WebSocket Handshake/Connection failed')
-                this.logger.error(`Encountered ws.onError`)
-                isClosed = true
-                resolveNext?.()
-            }
-            ws.onclose = (event: CloseEvent) => {
-                this.logger.error(
-                    `CLOSING WEBSOCKET CONNECTION code: ${event.code}, reason: ${event.reason}`
-                )
-                isClosed = true
-                resolveNext?.()
-            }
-
-            try {
-                while (true) {
-                    if (messageQueue.length === 0) {
-                        if (isClosed) break // Exit if closed and queue is empty
-                        await new Promise<void>((r) => (resolveNext = r))
-                    }
-
-                    if (streamError) throw streamError
-
-                    while (messageQueue.length > 0) {
-                        yield messageQueue.shift()!
-                    }
-
-                    if (isClosed && messageQueue.length === 0) break
-                }
-            } finally {
-                if (ws.readyState === WebSocket.OPEN) ws.close()
-            }
-        }
-
-        return generator.call(this)
-    }
-
     subscribeToUpdatesStreaming(
         beginExclusive: number,
         interfaceIds: string[],
@@ -241,19 +93,16 @@ export class WebSocketClient {
                 filter,
             }
 
-            this.logger.info(request, `DEBUGGING REQUEST`)
+            this.logger.debug(request, `v2/updates request`)
 
             const ws = new WebSocket(wsUpdatesUrl, this.protocol)
 
             ws.onopen = () => {
-                this.logger.info(
-                    `OPENING WEBSOCKET CONNECTION AND SENDING REQUEST`
-                )
                 ws.send(JSON.stringify(request))
             }
             ws.onmessage = (event) => {
                 messageQueue.push(JSON.parse(event.data as string))
-                this.logger.info(`Received event: ${event.data}`)
+                this.logger.debug(event.data, `Received event`)
                 resolveNext?.()
             }
             ws.onerror = () => {
@@ -263,7 +112,7 @@ export class WebSocketClient {
                 resolveNext?.()
             }
             ws.onclose = (event: CloseEvent) => {
-                this.logger.error(
+                this.logger.debug(
                     `CLOSING WEBSOCKET CONNECTION code: ${event.code}, reason: ${event.reason}`
                 )
                 isClosed = true
@@ -272,13 +121,15 @@ export class WebSocketClient {
 
             try {
                 while (true) {
+                    // If the queue is empty, we wait
                     if (messageQueue.length === 0) {
-                        if (isClosed) break // Exit if closed and queue is empty
+                        if (isClosed) break
                         await new Promise<void>((r) => (resolveNext = r))
                     }
 
                     if (streamError) throw streamError
 
+                    // Flush the queue to the consumer
                     while (messageQueue.length > 0) {
                         yield messageQueue.shift()!
                     }
@@ -287,188 +138,10 @@ export class WebSocketClient {
                 }
             } finally {
                 if (ws.readyState === WebSocket.OPEN) ws.close()
+                this.logger.info('Generator cleanup: WebSocket closed.')
             }
         }
 
         return generator.call(this)
-    }
-
-    async subscribeToActiveContracts(request: GetActiveContractsRequest) {
-        const wsUpdatesUrl = `${this.baseUrl}${CHANNELS.v2_state_active_contracts}`
-
-        return new Promise((resolve, reject) => {
-            const ws = new WebSocket(wsUpdatesUrl, [
-                `jwt.token.${this.token}`,
-                'daml.ws.auth',
-            ])
-            const results: JsGetActiveContractsResponse[] = []
-            let finished = false
-            let error: Error | null = null
-            setTimeout(() => {
-                if (!finished && !error && results.length === 0) {
-                    error = Error(
-                        `No data received from WebSocket ${wsUpdatesUrl} within 60ms`
-                    )
-                    reject(error)
-                    ws.close()
-                }
-            }, this.wsSupportBackOff)
-
-            ws.onopen = () => {
-                ws.send(JSON.stringify(request))
-            }
-
-            ws.onmessage = (event: MessageEvent<string>) => {
-                try {
-                    this.logger.info(`received event: ${event.data}`)
-                    results.push(JSON.parse(event.data))
-                } catch (err) {
-                    this.logger.error(`Invalid JSON ${err}`)
-                }
-            }
-
-            ws.onerror = () => {
-                error = new Error(`WebSocket error`)
-                reject(error)
-            }
-
-            ws.onclose = () => {
-                finished = true
-                if (!error) {
-                    resolve({
-                        updates: results,
-                    })
-                }
-            }
-        })
-    }
-
-    async subscribetoCompletions(
-        userId: string,
-        parties: PartyId[],
-        beginExclusive: number
-    ) {
-        const wsUpdatesUrl = `${this.baseUrl}${CHANNELS.v2_commands_completions}`
-
-        const request = {
-            beginExclusive,
-            userId,
-            parties,
-        }
-
-        return new Promise((resolve, reject) => {
-            const ws = new WebSocket(wsUpdatesUrl, this.protocol)
-            const results: CompletionStreamResponse[] = []
-            let finished = false
-            let error: Error | null = null
-            setTimeout(() => {
-                if (!finished && !error && results.length === 0) {
-                    error = Error(
-                        `No data received from WebSocket ${wsUpdatesUrl} within 60ms`
-                    )
-                    reject(error)
-                    ws.close()
-                }
-            }, this.wsSupportBackOff)
-
-            ws.onopen = () => {
-                ws.send(JSON.stringify(request))
-            }
-
-            ws.onmessage = (event: MessageEvent<string>) => {
-                try {
-                    this.logger.info(`received event: ${event.data}`)
-                    results.push(JSON.parse(event.data))
-                } catch (err) {
-                    this.logger.error(`Invalid JSON ${err}`)
-                }
-            }
-
-            ws.onerror = () => {
-                error = new Error(`WebSocket error`)
-                reject(error)
-            }
-
-            ws.onclose = () => {
-                finished = true
-                if (!error) {
-                    resolve({
-                        updates: results,
-                    })
-                }
-            }
-        })
-    }
-
-    async subscribeToUpdates(
-        beginExclusive: number,
-        interfaceNames: string[] | string,
-        endInclusive?: number,
-        partyId?: PartyId,
-        verbose: boolean = true
-    ) {
-        const wsUpdatesUrl = `${this.baseUrl}${CHANNELS.v2_updates}`
-
-        const filter = partyId
-            ? TransactionFilterBySetup(interfaceNames, {
-                  includeWildcard: true,
-                  isMasterUser: false,
-                  partyId,
-              })
-            : TransactionFilterBySetup(interfaceNames, {
-                  includeWildcard: true,
-                  isMasterUser: true,
-                  partyId,
-              })
-
-        const request = {
-            beginExclusive,
-            endInclusive,
-            verbose,
-            filter,
-        }
-
-        return new Promise((resolve, reject) => {
-            const ws = new WebSocket(wsUpdatesUrl, this.protocol)
-            const results: JsGetUpdatesResponse[] = []
-            let finished = false
-            let error: Error | null = null
-            setTimeout(() => {
-                if (!finished && !error && results.length === 0) {
-                    error = Error(
-                        `No data received from WebSocket ${wsUpdatesUrl} within 60ms`
-                    )
-                    reject(error)
-                    ws.close()
-                }
-            }, this.wsSupportBackOff)
-
-            ws.onopen = () => {
-                ws.send(JSON.stringify(request))
-            }
-
-            ws.onmessage = (event: MessageEvent<string>) => {
-                try {
-                    this.logger.info(`received event: ${event.data}`)
-                    results.push(JSON.parse(event.data))
-                } catch (err) {
-                    this.logger.error(`Invalid JSON ${err}`)
-                }
-            }
-
-            ws.onerror = () => {
-                error = new Error(`WebSocket error`)
-                reject(error)
-            }
-
-            ws.onclose = () => {
-                finished = true
-                if (!error) {
-                    resolve({
-                        updates: results,
-                    })
-                }
-            }
-        })
     }
 }
