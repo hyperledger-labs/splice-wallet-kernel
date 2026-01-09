fea9178e7d7a8d2a5d30cbc24c7cf09260c02dea
Pawel Stepien <pawel.stepien@digitalasset.com>
2026-01-09 14:02:44 +0100

    migration
    Signed-off-by: Pawel Stepien <pawel.stepien@digitalasset.com>

---
+++ core/wallet-store-sql/src/migrations/005-add-wallet-disabled-reason.ts
@@ -0,0 +1,20 @@
+// Copyright (c) 2025-2026 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import { Kysely } from 'kysely'
+import { DB } from '../schema.js'
+
+export async function up(db: Kysely<DB>): Promise<void> {
+    await db.schema
+        .alterTable('wallets')
+        .addColumn('disabled', 'boolean')
+        .execute()
+
+    await db.schema.alterTable('wallets').addColumn('reason', 'text').execute()
+}
+
+export async function down(db: Kysely<DB>): Promise<void> {
+    await db.schema.alterTable('wallets').dropColumn('reason').execute()
+
+    await db.schema.alterTable('wallets').dropColumn('disabled').execute()
+}
