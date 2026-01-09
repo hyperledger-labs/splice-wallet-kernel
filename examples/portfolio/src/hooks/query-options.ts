b01ca8a5742f9a1b1e68c998b47dae7188403e2a
Jasper Van der Jeugt <jasper.vanderjeugt@digitalasset.com>
2026-01-09 12:42:53 +0100

    Add copyright headers
    Signed-off-by: Jasper Van der Jeugt <jasper.vanderjeugt@digitalasset.com>

--- examples/portfolio/src/hooks/query-options.ts
+++ examples/portfolio/src/hooks/query-options.ts
@@ -1,3 +1,6 @@
+// Copyright (c) 2025-2026 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 import { queryOptions } from '@tanstack/react-query'
 import { listPendingTransfers } from '../services/portfolio-service-implementation'
 import { queryKeys } from './query-keys'
--- examples/portfolio/src/hooks/useExerciseTransfer.ts
+++ examples/portfolio/src/hooks/useExerciseTransfer.ts
@@ -1,3 +1,6 @@
+// Copyright (c) 2025-2026 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 import { PartyId } from '@canton-network/core-types'
 import { useQueryClient, useMutation } from '@tanstack/react-query'
 import { usePortfolio } from '../contexts/PortfolioContext'
