33e651384b20b654f3e0704ffbecea2d95b73734
pawelstepien-da <pawel.stepien@digitalasset.com>
2026-01-09 10:46:18 +0100

    feat: refactor splice provider (#1097)
    Signed-off-by: Pawel Stepien <pawel.stepien@digitalasset.com>

--- core/splice-provider/src/SpliceProviderWindow.ts
+++ core/splice-provider/src/SpliceProviderWindow.ts
@@ -1,19 +1,26 @@
 // Copyright (c) 2025-2026 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import { RequestPayload, WindowTransport } from '@canton-network/core-types'
+import { RequestPayload } from '@canton-network/core-types'
+import { WindowTransport } from '@canton-network/core-rpc-transport'
+import SpliceWalletJSONRPCDAppAPI from '@canton-network/core-wallet-dapp-rpc-client'
 import { SpliceProviderBase } from './SpliceProvider.js'
 
 export class SpliceProviderWindow extends SpliceProviderBase {
-    private transport: WindowTransport
+    private client: SpliceWalletJSONRPCDAppAPI
 
     constructor() {
         super()
-        this.transport = new WindowTransport(window)
+        const transport = new WindowTransport(window)
+        this.client = new SpliceWalletJSONRPCDAppAPI(transport)
     }
 
     public async request<T>({ method, params }: RequestPayload): Promise<T> {
-        const response = await this.transport.submit({ method, params })
-        return response.result as T
+        return (await (
+            this.client.request as (
+                method: string,
+                params?: RequestPayload['params']
+            ) => Promise<unknown>
+        )(method, params)) as T
     }
 }
