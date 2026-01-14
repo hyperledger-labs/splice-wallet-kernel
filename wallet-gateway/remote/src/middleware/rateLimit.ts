7d4ce7b1be7b11eaf021338afac4ef12059d16a4
Jasper Van der Jeugt <jasper.vanderjeugt@digitalasset.com>
2026-01-14 18:04:47 +0100

    Raise rateLimit only in CI
    Signed-off-by: Jasper Van der Jeugt <jasper.vanderjeugt@digitalasset.com>

--- wallet-gateway/remote/src/middleware/rateLimit.ts
+++ wallet-gateway/remote/src/middleware/rateLimit.ts
@@ -5,7 +5,7 @@ import rateLimit from 'express-rate-limit'
 
 export const rpcRateLimit = rateLimit({
     windowMs: 1 * 60 * 1000, // 1 minute
-    max: 1000, // limit each IP to 100 requests per windowMs
+    max: process.env.CI ? 10000 : 100, // limit each IP to 100 requests per windowMs, more when running in CI
     standardHeaders: true,
     legacyHeaders: false,
 })
