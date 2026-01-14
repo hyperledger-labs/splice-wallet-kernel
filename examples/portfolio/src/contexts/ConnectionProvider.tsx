a43c3f1875376cef6eaf0e1173b309712421180b
Jasper Van der Jeugt <jasper.vanderjeugt@digitalasset.com>
2026-01-14 11:21:35 +0100

    Add some debugging
    Signed-off-by: Jasper Van der Jeugt <jasper.vanderjeugt@digitalasset.com>

--- examples/portfolio/src/contexts/ConnectionProvider.tsx
+++ examples/portfolio/src/contexts/ConnectionProvider.tsx
@@ -79,6 +79,7 @@ export const ConnectionProvider: React.FC<{ children: React.ReactNode }> = ({
         const messageListener = async (event: sdk.dappAPI.TxChangedEvent) => {
             console.log('incoming event', event)
             if (event.status === 'executed') {
+                console.log("invalidating caches, offset: ", event.payload.completionOffset)
                 await queryClient.invalidateQueries({
                     queryKey: ['listPendingTransfers'],
                 })
