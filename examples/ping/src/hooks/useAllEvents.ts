556ab6a74d53ac8f40dceff36b79b6b5145a9695
phillip olesen <phillip.olesen@digitalasset.com>
2026-01-29 10:43:23 +0100

    huh
    Signed-off-by: phillip olesen <phillip.olesen@digitalasset.com>

--- examples/ping/src/hooks/useAllEvents.ts
+++ examples/ping/src/hooks/useAllEvents.ts
@@ -61,7 +61,7 @@ export function useAllEvents() {
                 sdk.removeOnAccountsChanged(accountsListener)
             }
         }
-    }, [window.canton])
+    }, [])
 
     return events
 }
