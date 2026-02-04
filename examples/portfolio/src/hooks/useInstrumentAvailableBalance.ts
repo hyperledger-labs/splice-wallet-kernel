92e0cf2527581546a439f484a7567f98577b2840
Fayi <112705750+fayi-da@users.noreply.github.com>
2026-02-04 12:13:24 +0000

    feat(example-portfolio): add transaction history table to wallet page (#1234)
    Signed-off-by: fayi-da <fayimora.femibalogun@digitalasset.com>

--- examples/portfolio/src/hooks/useInstrumentAvailableBalance.ts
+++ examples/portfolio/src/hooks/useInstrumentAvailableBalance.ts
@@ -1,3 +1,6 @@
+// Copyright (c) 2025-2026 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 import type { InstrumentId } from '@canton-network/core-token-standard'
 import type { AggregatedHolding } from '../utils/aggregate-holdings'
 import { useAggregatedHoldings } from './useAggregatedHoldings'
