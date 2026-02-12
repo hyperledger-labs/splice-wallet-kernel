83df996c7ee1276fc9aeb5027937efa633da9726
rukmini-basu-da <126689545+rukmini-basu-da@users.noreply.github.com>
2026-02-12 13:38:21 -0500

    feat: sdk v1 initialization + tests (#1298)
    * sdk v1 initialization and tests
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * sdk v1 initialization and tests
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * sdk v1 initialization and tests
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * sdk v1 initialization and tests
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * sdk v1 initialization and tests
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    ---------
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>

--- sdk/wallet-sdk/src/v1/keys/client.ts
+++ sdk/wallet-sdk/src/v1/keys/client.ts
@@ -1,7 +1,7 @@
 // Copyright (c) 2025-2026 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import { createKeyPair } from '@canton-network/core-signing-lib'
+import { createKeyPair, KeyPair } from '@canton-network/core-signing-lib'
 
 export class KeysClient {
     constructor() {}
@@ -10,7 +10,7 @@ export class KeysClient {
      *
      * @returns A base64 encoded public/private key pair
      */
-    generate() {
+    generate(): KeyPair {
         return createKeyPair()
     }
 }
