de0e44af8d8d431e61b6ec16ccd9d5011d259dc3
Mateusz Piątkowski <mateusz.piatkowski@digitalasset.com>
2026-02-16 10:37:27 +0100

    refactor(wallet-sdk): cleanup code
    Signed-off-by: Mateusz Piątkowski <mateusz.piatkowski@digitalasset.com>

--- sdk/wallet-sdk/src/v1/party/internalClient.ts
+++ sdk/wallet-sdk/src/v1/party/internalClient.ts
@@ -1,11 +1,6 @@
 // Copyright (c) 2025-2026 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import { PartyClient } from './types'
-
-export default class InternalPartyClient extends PartyClient {
-    protected partyMode = 'internal' as const
-    constructor() {
-        super()
-    }
+export default class InternalPartyClient {
+    constructor() {}
 }
