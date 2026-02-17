044b91913cc7864216e69ba9bf1e10d258985be3
mateuszpiatkowski-da <mateusz.piatkowski@digitalasset.com>
2026-02-17 14:56:00 +0100

    feat(wallet-sdk): create external party v1 (#1308)
    Signed-off-by: Mateusz Piątkowski <mateusz.piatkowski@digitalasset.com>

--- docs/wallet-integration-guide/examples/scripts/v1/01-init.ts
+++ docs/wallet-integration-guide/examples/scripts/v1/01-init.ts
@@ -8,15 +8,26 @@ import { pino } from 'pino'
 
 const logger = pino({ name: 'v1-initialization', level: 'info' })
 
+const localNetAuth = localNetAuthDefault(logger)
+const userId = localNetAuth.userId!
+
 const sdk = await Sdk.create({
     logger,
-    authTokenProvider: new AuthTokenProvider(localNetAuthDefault(logger)),
+    authTokenProvider: new AuthTokenProvider(localNetAuth),
     ledgerClientUrl: localNetStaticConfig.LOCALNET_APP_USER_LEDGER_URL,
     validatorUrl: localNetStaticConfig.LOCALNET_SCAN_PROXY_API_URL,
     tokenStandardUrl: localNetStaticConfig.LOCALNET_TOKEN_STANDARD_URL,
+    scanApiBaseUrl: localNetStaticConfig.LOCALNET_SCAN_PROXY_API_URL,
     registries: [localNetStaticConfig.LOCALNET_REGISTRY_API_URL],
 })
 
-sdk.keys.generate()
+const aliceKeys = sdk.keys.generate()
 
-//TODO: add external party creation (#1294)
+const alice = await sdk.party.external
+    .create(aliceKeys.publicKey, {
+        partyHint: 'aliceInWonderland',
+    })
+    .sign(aliceKeys.privateKey)
+    .execute(userId)
+
+logger.info({ alice }, 'Alice party representation:')
