413a382c9c47f5f8424b92291e4fc47a1aeee93a
Mateusz Piątkowski <mateusz.piatkowski@digitalasset.com>
2026-03-11 12:05:42 +0100

    fix(docs-wallet-integration-guide-examples): fix build
    Signed-off-by: Mateusz Piątkowski <mateusz.piatkowski@digitalasset.com>

--- docs/wallet-integration-guide/examples/scripts/v1/02-two-step-transfer/types.ts
+++ docs/wallet-integration-guide/examples/scripts/v1/02-two-step-transfer/types.ts
@@ -1,12 +1,12 @@
+import { GenerateTransactionResponse } from '@canton-network/core-ledger-client'
 import { KeyPair } from '@canton-network/core-signing-lib'
 import { Sdk } from '@canton-network/wallet-sdk'
 import pino from 'pino'
-import { createParties } from '../common/createParties.js'
 
 export type TransferTestScriptParameters = {
     sdk: Sdk
-    sender: Awaited<ReturnType<typeof createParties>>['sender']
-    receiver: Awaited<ReturnType<typeof createParties>>['receiver']
+    sender: GenerateTransactionResponse
+    receiver: GenerateTransactionResponse
     senderKeys: KeyPair
     receiverKeys: KeyPair
     logger: pino.Logger
