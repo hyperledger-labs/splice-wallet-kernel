c4462ad9cd6f2fc09027a491e5fcbbcbca57455a
rukmini-basu-da <rukmini.basu@digitalasset.com>
2026-03-12 09:35:35 -0400

    add acs reader
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>

--- sdk/wallet-sdk/src/v1/namespace/ledger/client.ts
+++ sdk/wallet-sdk/src/v1/namespace/ledger/client.ts
@@ -9,6 +9,7 @@ import { PreparedTransaction } from '../transactions/prepared.js'
 import { SignedTransaction } from '../transactions/signed.js'
 import { Ops } from '@canton-network/core-provider-ledger'
 import { Dar } from './dar/client.js'
+import { AcsOptions } from '@canton-network/core-acs-reader'
 
 export class Ledger {
     public readonly dar: Dar
@@ -136,6 +137,22 @@ export class Ledger {
                 },
             }
         )
+    }
+
+    /**
+     *
+     * @param options AcsOptions for querying the Active Contract Set (ACS).
+     * offset: The ledger offset at which to query the ACS. This is a required field.
+     * templateIds: An optional array of template IDs to filter the ACS by. If not provided, no filtering by template ID will be applied.
+     * parties: An optional array of party IDs to filter the ACS by. If not provided, no filtering by party will be applied.
+     * filterByParty: A boolean flag indicating whether to apply party-based filtering. If true, the query will filter contracts based on the specified parties. If false or not provided, party-based filtering will not be applied.
+     * interfaceIds: An optional array of interface IDs to filter the ACS by. If not provided, no filtering by interface ID will be applied.
+     * limit: An optional number specifying the maximum number of active contracts to return in a single query. If not provided, the default limit will be determined by the ledger API.
+     * continueUntilCompletion: A boolean flag indicating whether to continue polling the ledger until the query is complete. If true, the method will repeatedly query the ledger until all matching active contracts have been retrieved. If false or not provided, the method will return after a single query, which may return a
+     * @returns Active contracts matching the provided query options.
+     */
+    async readAcs(options: AcsOptions) {
+        return this.sdkContext.acsReader.getActiveContracts(options)
     }
 
     /**
