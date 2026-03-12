98a00569d795a00b83b2adc9d66fcda757922b19
mateuszpiatkowski-da <mateusz.piatkowski@digitalasset.com>
2026-03-12 11:50:48 +0100

    feat(wallet-sdk): amulet preapproval - fetch & create command v1 (#1422)
    Signed-off-by: Mateusz Piątkowski <mateusz.piatkowski@digitalasset.com>

--- sdk/wallet-sdk/src/v1/namespace/amulet/types.ts
+++ sdk/wallet-sdk/src/v1/namespace/amulet/types.ts
@@ -1,6 +1,13 @@
 // Copyright (c) 2025-2026 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+import { PartyId } from '@canton-network/core-types'
+
+export type PreapprovalParties = {
+    receiver: PartyId
+    provider?: PartyId
+}
+
 export type FeaturedAppRight = {
     template_id: string
     contract_id: string
