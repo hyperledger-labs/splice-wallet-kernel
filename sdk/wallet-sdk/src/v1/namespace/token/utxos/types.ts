65ad2312ceb129320741f3419c952cdb1499576d
rukmini-basu-da <rukmini.basu@digitalasset.com>
2026-03-17 12:15:43 -0400

    refactor token namespace
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>

--- sdk/wallet-sdk/src/v1/namespace/token/utxos/types.ts
+++ sdk/wallet-sdk/src/v1/namespace/token/utxos/types.ts
@@ -2,8 +2,15 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { PartyId } from '@canton-network/core-types'
-import { Metadata } from '@canton-network/core-token-standard'
 import { Holding, PrettyContract } from '@canton-network/core-tx-parser'
+
+export type MergeUtxosParams = {
+    partyId: PartyId
+    inputUtxos?: PrettyContract<Holding>[]
+    nodeLimit?: number
+    memo?: string
+}
+
 /**
  * @param includeLocked defaulted to true, this will include locked UTXOs.
  * @param limit optional limit for number of UTXOs to return.
@@ -17,28 +24,4 @@ export type ListHoldingsParams = {
     limit?: number
     offset?: number
     continueUntilCompletion?: boolean
-}
-
-export type TransferParams = {
-    sender: PartyId
-    recipient: PartyId
-    amount: string
-    instrumentId: string
-    registryUrl: URL
-    inputUtxos?: string[]
-    expirationDate?: Date
-    meta?: Metadata
-    memo?: string
-}
-
-export type TransferAllocationChoiceParams = {
-    transferInstructionCid: string
-    registryUrl: URL
-}
-
-export type MergeUtxosParams = {
-    partyId: PartyId
-    inputUtxos?: PrettyContract<Holding>[]
-    nodeLimit?: number
-    memo?: string
 }
