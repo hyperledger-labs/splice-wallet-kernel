df88cd9a4a2350b009ff1e7132232134193b15a1
Mateusz Piątkowski <mateusz.piatkowski@digitalasset.com>
2026-03-23 14:41:44 +0100

    feat(wallet-sdk): :building_construction: add encoder, add converter
    Signed-off-by: Mateusz Piątkowski <mateusz.piatkowski@digitalasset.com>

--- sdk/wallet-sdk/src/v1/namespace/ledger/hash/service.ts
+++ sdk/wallet-sdk/src/v1/namespace/ledger/hash/service.ts
@@ -4,10 +4,11 @@
 import {
     DamlTransaction,
     DamlTransaction_Node,
-    HashingSchemeVersion,
     PreparedTransaction as PreparedTransactionForHash,
 } from '@canton-network/core-ledger-proto'
 import { WalletSdkContext } from 'src/v1/sdk'
+import { Converter } from './converter'
+import { Encoder } from './encoder'
 
 export class HashService {
     constructor(private readonly ctx: WalletSdkContext) {}
@@ -28,108 +29,48 @@ export class HashService {
             preparedTransaction.transaction!,
             nodesDict
         )
-        const metadataHash = await this.sha256(
-            await this.mkByteArray(
-                this.preparedTransactionHashPurpose,
-                await encodeMetadata(preparedTransaction.metadata)
+        const metadataHash = await Encoder.sha256(
+            Encoder.concatBytes(
+                Encoder.preparedTransactionHashPurpose,
+                await Encoder.encodeMetadata(preparedTransaction.metadata!)
             )
         )
 
-        const msg = await this.mkByteArray(
-            this.preparedTransactionHashPurpose,
-            this.hashingSchemeVersion,
+        const msg = Encoder.concatBytes(
+            Encoder.preparedTransactionHashPurpose,
+            Encoder.hashingSchemeVersion,
             transactionHash,
             metadataHash
         )
 
-        const arrayBufferHash = await this.sha256(msg)
+        const arrayBufferHash = await Encoder.sha256(msg)
+        const converted = new Converter(arrayBufferHash)
         switch (format) {
             case 'hex':
-                return this.toHex(arrayBufferHash)
+                return converted.toHex()
             case 'base64':
             default:
-                return this.toBase64(arrayBufferHash)
+                return converted.toBase64()
         }
-    }
-
-    private async sha256(message: string | Uint8Array) {
-        const msg =
-            typeof message === 'string'
-                ? new TextEncoder().encode(message)
-                : message
-
-        return crypto.subtle
-            .digest('SHA-256', new Uint8Array(msg))
-            .then((hash) => new Uint8Array(hash))
-    }
-
-    private toHex(bytes: Uint8Array): string {
-        return Array.from(bytes)
-            .map((byte) => byte.toString(16).padStart(2, '0'))
-            .join('')
-    }
-
-    private toBase64(data: Uint8Array): string {
-        let binaryString = ''
-        const len = data.byteLength
-        for (let i = 0; i < len; i++) {
-            binaryString += String.fromCharCode(data[i])
-        }
-        return btoa(binaryString)
     }
 
     private async hashTransaction(
         transaction: DamlTransaction,
         nodesDict: Record<string, DamlTransaction_Node>
     ): Promise<Uint8Array> {
-        const encodedTransaction = await encodeTransaction(
+        const encodedTransaction = await Encoder.encodeTransaction(
             transaction,
             nodesDict,
             transaction.nodeSeeds
         )
 
-        const hash = await this.sha256(
-            await this.mkByteArray(
-                this.preparedTransactionHashPurpose,
+        const hash = await Encoder.sha256(
+            Encoder.concatBytes(
+                Encoder.preparedTransactionHashPurpose,
                 encodedTransaction
             )
         )
 
         return hash
-    }
-
-    private readonly preparedTransactionHashPurpose = Uint8Array.from([
-        0x00, 0x00, 0x00, 0x30,
-    ])
-
-    private readonly hashingSchemeVersion = Uint8Array.from([
-        HashingSchemeVersion.V2,
-    ])
-
-    private async mkByteArray(
-        ...args: (number | Uint8Array)[]
-    ): Promise<Uint8Array> {
-        const normalizedArgs: Uint8Array[] = args.map((arg) => {
-            if (typeof arg === 'number') {
-                return new Uint8Array([arg])
-            } else {
-                return arg
-            }
-        })
-
-        let totalLength = 0
-        normalizedArgs.forEach((arg) => {
-            totalLength += arg.length
-        })
-
-        const mergedArray = new Uint8Array(totalLength)
-        let offset = 0
-
-        normalizedArgs.forEach((arg) => {
-            mergedArray.set(arg, offset)
-            offset += arg.length
-        })
-
-        return mergedArray
     }
 }
