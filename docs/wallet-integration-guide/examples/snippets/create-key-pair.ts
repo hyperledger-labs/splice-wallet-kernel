e4a9cee9451894a69465420a2baa66e37d676115
rukmini-basu-da <rukmini.basu@digitalasset.com>
2026-03-30 12:37:45 -0400

    add at least one snippet
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>

---
+++ docs/wallet-integration-guide/examples/snippets/create-key-pair.ts
@@ -0,0 +1,9 @@
+import { SDK, localNetStaticConfig } from '@canton-network/wallet-sdk'
+
+export default async function () {
+    const sdk = await SDK.create({
+        auth: global.TOKEN_PROVIDER_CONFIG_DEFAULT,
+        ledgerClientUrl: localNetStaticConfig.LOCALNET_APP_USER_LEDGER_URL,
+    })
+    const key = sdk.keys.generate()
+}
