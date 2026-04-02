5cf385970a47192cf9f9fe306e3f30eeaa19a447
Phillip Olesen <phillip.olesen@digitalasset.com>
2026-04-02 13:38:59 +0200

    for all the prettier ladies
    Signed-off-by: Phillip Olesen <phillip.olesen@digitalasset.com>

--- examples/ping/playwright.config.ts
+++ examples/ping/playwright.config.ts
@@ -40,7 +40,7 @@ export default defineConfig({
         {
             name: 'chromium',
             use: {
-                ...devices['Desktop Chrome']
+                ...devices['Desktop Chrome'],
             },
         },
 
