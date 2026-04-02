d603e8b5836541257f81257c2c5c3384e46372cd
Pawel Stepien <pawel.stepien@digitalasset.com>
2026-04-02 10:57:07 +0200

    remote wg vitest
    Signed-off-by: Pawel Stepien <pawel.stepien@digitalasset.com>

--- core/wallet-store-sql/vitest.config.ts
+++ core/wallet-store-sql/vitest.config.ts
@@ -1,8 +1,6 @@
 // Copyright (c) 2025-2026 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-// Native modules (better-sqlite3) require Node; browser project is omitted.
-
 import { defineConfig, defineProject } from 'vitest/config'
 
 export default defineConfig({
