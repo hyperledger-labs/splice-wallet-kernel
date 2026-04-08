ed99f139f5718ea5c84aa5649717e83ffd9f81b6
rukmini-basu-da <rukmini.basu@digitalasset.com>
2026-04-08 11:13:43 -0400

    remove scan proxy
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>

--- sdk/wallet-sdk/src/config.ts
+++ sdk/wallet-sdk/src/config.ts
@@ -1,7 +1,7 @@
 // Copyright (c) 2025-2026 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-const LOCALNET_SCAN_PROXY_API_URL = new URL(
+const LOCALNET_APP_VALIDATOR_URL = new URL(
     'http://localhost:2000/api/validator'
 )
 
@@ -13,12 +13,12 @@ const LOCALNET_TOKEN_STANDARD_URL = new URL('http://localhost:5003')
 
 //scan proxy exposes the registry endpoints as well
 const LOCALNET_REGISTRY_API_URL = new URL(
-    LOCALNET_SCAN_PROXY_API_URL.href + '/v0/scan-proxy'
+    LOCALNET_APP_VALIDATOR_URL.href + '/v0/scan-proxy'
 )
 const LOCALNET_USER_ID = 'ledger-api-user'
 
 export const localNetStaticConfig = {
-    LOCALNET_SCAN_PROXY_API_URL,
+    LOCALNET_APP_VALIDATOR_URL,
     LOCALNET_SCAN_API_URL,
     LOCALNET_REGISTRY_API_URL,
     LOCALNET_APP_USER_LEDGER_URL,
