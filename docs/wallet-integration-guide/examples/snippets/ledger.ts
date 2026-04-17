abbe14dcecbd8215aa8b728128a9b8ca7fea8d5c
Mateusz Piątkowski <mateusz.piatkowski@digitalasset.com>
2026-04-17 13:22:36 +0200

    fix: fix build
    Signed-off-by: Mateusz Piątkowski <mateusz.piatkowski@digitalasset.com>

--- docs/wallet-integration-guide/examples/snippets/ledger.ts
+++ docs/wallet-integration-guide/examples/snippets/ledger.ts
@@ -2,18 +2,19 @@ import { localNetStaticConfig, SDK } from '@canton-network/wallet-sdk'
 
 export default async function () {
     const sdk = await SDK.create({
-        auth: TOKEN_PROVIDER_CONFIG_DEFAULT,
+        auth: global.TOKEN_PROVIDER_CONFIG_DEFAULT,
         ledgerClientUrl: localNetStaticConfig.LOCALNET_APP_USER_LEDGER_URL,
+        amulet: global.AMULET_NAMESPACE_CONFIG,
     })
 
     const partyId = EXISTING_PARTY_1
     const privateKey = EXISTING_PARTY_1_KEYS.privateKey
 
-    const commands = PREPARED_COMMAND
+    const [commands, disclosedContracts] = await sdk.amulet.tap(partyId, '200')
 
     // ledger namespace is immediately available
     await sdk.ledger
-        .prepare({ partyId, commands })
+        .prepare({ partyId, commands, disclosedContracts })
         .sign(privateKey)
         .execute({ partyId })
 }
