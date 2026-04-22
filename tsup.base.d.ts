4299bddd5fbc0d11bad948595db299b6cbbf1dbd
Alex Matson <alex.matson@digitalasset.com>
2026-04-22 14:29:39 -0400

    rm
    Signed-off-by: Alex Matson <alex.matson@digitalasset.com>

--- tsup.base.d.ts
+++
@@ -1,6 +0,0 @@
-// Copyright (c) 2025-2026 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-import type { Options } from 'tsup'
-export declare const base: Options
-//# sourceMappingURL=tsup.base.d.ts.map
