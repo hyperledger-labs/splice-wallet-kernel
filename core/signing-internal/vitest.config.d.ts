a0368d8b517d87f7e8a5b0fd9eef2ab7738c5034
Alex Matson <alex.matson@digitalasset.com>
2026-04-22 14:26:56 -0400

    fix
    Signed-off-by: Alex Matson <alex.matson@digitalasset.com>

--- core/signing-internal/vitest.config.d.ts
+++
@@ -1,6 +0,0 @@
-// Copyright (c) 2025-2026 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-declare const _default: import('vite').UserConfig
-export default _default
-//# sourceMappingURL=vitest.config.d.ts.map
