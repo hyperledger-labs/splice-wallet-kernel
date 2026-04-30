516144b6d823ac2115145c8ac1f6cc55bd466e17
rukmini-basu-da <126689545+rukmini-basu-da@users.noreply.github.com>
2026-04-30 07:33:25 -0400

    chore: core-amulet-service test (#1691)
    * amulet core unit test
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * unit tests
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * unit tests
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * more unit tests
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * amulet service unit tests
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * more unit tests
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * unit tests
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * cleanup
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * amulet service tests
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * cleanup
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * cleanup
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * cleanup
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * cleanup
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * fix tests
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * fix tests
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * moar tests
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * update coverage
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    ---------
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>

---
+++ core/amulet-service/src/amulet-service-consts.test.ts
@@ -0,0 +1,123 @@
+// Copyright (c) 2025-2026 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+export const amuletRules = {
+    template_id:
+        '6c5802f86709a0ad4784af81f0bab40f3070b2f58128d8843da1e1784c147802:Splice.AmuletRules:AmuletRules',
+    contract_id:
+        '00013916345ad89e9318d61d5b3654657d8f9bd14a2e3118bd8ed568389bc4decdca1212202c1ebb5a0bc92ede452b506eb55d463bf89343ed7797d9394b88154f21e4bcee',
+    payload: {
+        dso: 'DSO::1220eba34d13ab223f1a933fbde4e760dff9a3a965031151b0918ca9739424406ded',
+
+        isDevNet: true,
+        contractStateSchemaVersion: null,
+    },
+    created_event_blob:
+        'CgMyLjESsQ4KRQABORY0WtiekxjWHVs2VGV9j5vRSi4xGL2O1Wg4m8TezcoSEiAsHrtaC8ku3kUrUG61XUY7+JND7XeX2TlLiBVPIeS87hINc3BsaWNlLWFtdWxldBpkCkA2YzU4MDJmODY3MDlhMGFkNDc4NGFmODFmMGJhYjQwZjMwNzBiMmY1ODEyOGQ4ODQzZGExZTE3ODRjMTQ3ODAyEgZTcGxpY2USC0FtdWxldFJ1bGVzGgtBbXVsZXRSdWxlcyLyC2rvCwpNCks6SURTTzo6MTIyMGViYTM0ZDEzYWIyMjNmMWE5MzNmYmRlNGU3NjBkZmY5YTNhOTY1MDMxMTUxYjA5MThjYTk3Mzk0MjQ0MDZkZWQKlwsKlAtqkQsKiAsKhQtqggsKkgEKjwFqjAEKFgoUahIKEAoOMgwwLjAwMDAwMDAwMDAKFgoUahIKEAoOMgwwLjAwMDAxOTAyNTkKHAoaahgKEAoOMgwwLjAwMDAwMDAwMDAKBAoCWgAKFgoUahIKEAoOMgwwLjAwMDAwMDAwMDAKEAoOMgwxLjAwMDAwMDAwMDAKBQoDGMgBCgUKAxjIAQoECgIYZArhBgreBmrbBgqUAQqRAWqOAQoaChgyFjQwMDAwMDAwMDAwLjAwMDAwMDAwMDAKEAoOMgwwLjA1MDAwMDAwMDAKEAoOMgwwLjE1MDAwMDAwMDAKEAoOMgwwLjIwMDAwMDAwMDAKEgoQMg4xMDAuMDAwMDAwMDAwMAoQCg4yDDAuNjAwMDAwMDAwMAoUChJSEAoOMgwyLjg1MDAwMDAwMDAKwQUKvgVauwUKrAFqqQEKEAoOagwKCgoIGIDAz+DolQcKlAEKkQFqjgEKGgoYMhYyMDAwMDAwMDAwMC4wMDAwMDAwMDAwChAKDjIMMC4xMjAwMDAwMDAwChAKDjIMMC40MDAwMDAwMDAwChAKDjIMMC4yMDAwMDAwMDAwChIKEDIOMTAwLjAwMDAwMDAwMDAKEAoOMgwwLjYwMDAwMDAwMDAKFAoSUhAKDjIMMi44NTAwMDAwMDAwCqwBaqkBChAKDmoMCgoKCBiAwO6husEVCpQBCpEBao4BChoKGDIWMTAwMDAwMDAwMDAuMDAwMDAwMDAwMAoQCg4yDDAuMTgwMDAwMDAwMAoQCg4yDDAuNjIwMDAwMDAwMAoQCg4yDDAuMjAwMDAwMDAwMAoSChAyDjEwMC4wMDAwMDAwMDAwChAKDjIMMC42MDAwMDAwMDAwChQKElIQCg4yDDIuODUwMDAwMDAwMAqrAWqoAQoQCg5qDAoKCggYgICbxpfaRwqTAQqQAWqNAQoZChcyFTUwMDAwMDAwMDAuMDAwMDAwMDAwMAoQCg4yDDAuMjEwMDAwMDAwMAoQCg4yDDAuNjkwMDAwMDAwMAoQCg4yDDAuMjAwMDAwMDAwMAoSChAyDjEwMC4wMDAwMDAwMDAwChAKDjIMMC42MDAwMDAwMDAwChQKElIQCg4yDDIuODUwMDAwMDAwMAqsAWqpAQoRCg9qDQoLCgkYgIC2jK+0jwEKkwEKkAFqjQEKGQoXMhUyNTAwMDAwMDAwLjAwMDAwMDAwMDAKEAoOMgwwLjIwMDAwMDAwMDAKEAoOMgwwLjc1MDAwMDAwMDAKEAoOMgwwLjIwMDAwMDAwMDAKEgoQMg4xMDAuMDAwMDAwMDAwMAoQCg4yDDAuNjAwMDAwMDAwMAoUChJSEAoOMgwyLjg1MDAwMDAwMDAKjQIKigJqhwIKZwplamMKYQpfYl0KWwpVQlNnbG9iYWwtZG9tYWluOjoxMjIwZWJhMzRkMTNhYjIyM2YxYTkzM2ZiZGU0ZTc2MGRmZjlhM2E5NjUwMzExNTFiMDkxOGNhOTczOTQyNDQwNmRlZBICCgAKVwpVQlNnbG9iYWwtZG9tYWluOjoxMjIwZWJhMzRkMTNhYjIyM2YxYTkzM2ZiZGU0ZTc2MGRmZjlhM2E5NjUwMzExNTFiMDkxOGNhOTczOTQyNDQwNmRlZApDCkFqPwocChpqGAoGCgQYgOowCg4KDGoKCggKBhiAsLT4CAoRCg8yDTE2LjY3MDAwMDAwMDAKBAoCGAgKBgoEGIC1GAoOCgxqCgoICgYYgJiavAQKSwpJakcKCgoIQgYwLjEuMTcKCgoIQgYwLjEuMTgKCgoIQgYwLjEuMjMKCQoHQgUwLjEuNgoKCghCBjAuMS4xOAoKCghCBjAuMS4xNwoECgJSAAoUChJSEAoOMgwxLjAwMDAwMDAwMDAKBAoCWgAKBAoCEAEqSURTTzo6MTIyMGViYTM0ZDEzYWIyMjNmMWE5MzNmYmRlNGU3NjBkZmY5YTNhOTY1MDMxMTUxYjA5MThjYTk3Mzk0MjQ0MDZkZWQ5Op7j0YVQBgBCKgomCiQIARIgj5OgbF15HUhXoH8PWj6HH6nKUBo9s2EIeHdNT+8n7U8QHg==',
+    created_at: '2026-04-28T14:12:22.491706Z',
+
+    domain_id:
+        'global-domain::1220eba34d13ab223f1a933fbde4e760dff9a3a965031151b0918ca9739424406ded',
+}
+
+export const activeRoundNormalized = {
+    template_id:
+        '6c5802f86709a0ad4784af81f0bab40f3070b2f58128d8843da1e1784c147802:Splice.Round:OpenMiningRound',
+    contract_id:
+        '009d63476d7323e7369d2c7a8b6f444e0b672388814786fcc7bd042dd7f5b86fdfca1212201666a477e90f00839c553108f86e749e6de3268568fe1fb3b73ff2f8f4d1aba7',
+    payload: {
+        dso: 'DSO::1220eba34d13ab223f1a933fbde4e760dff9a3a965031151b0918ca9739424406ded',
+        tickDuration: {
+            microseconds: '600000000',
+        },
+        issuingFor: {
+            microseconds: '12000000000',
+        },
+        amuletPrice: '0.005',
+        issuanceConfig: {
+            validatorRewardPercentage: '0.05',
+            optDevelopmentFundPercentage: null,
+            unfeaturedAppRewardCap: '0.6',
+            appRewardPercentage: '0.15',
+            featuredAppRewardCap: '100.0',
+            amuletToIssuePerYear: '40000000000.0',
+            validatorRewardCap: '0.2',
+            optValidatorFaucetCap: '2.85',
+        },
+        opensAt: '2026-04-28T18:25:45.568596Z',
+        transferConfigUsd: {
+            holdingFee: {
+                rate: '0.0000190259',
+            },
+            extraFeaturedAppRewardAmount: '1.0',
+            maxNumInputs: '100',
+            lockHolderFee: {
+                fee: '0.0',
+            },
+            createFee: {
+                fee: '0.0',
+            },
+            maxNumLockHolders: '50',
+            transferFee: {
+                initialRate: '0.0',
+                steps: [],
+            },
+            maxNumOutputs: '100',
+        },
+        targetClosesAt: '2026-04-28T18:45:45.568596Z',
+        round: {
+            number: '20',
+        },
+    },
+    created_event_blob:
+        'CgMyLjESkwYKRQCdY0dtcyPnNp0seotvRE4LZyOIgUeG/Me9BC3X9bhv38oSEiAWZqR36Q8Ag5xVMQj4bnSebeMmhWj+H7O3P/L49NGrpxINc3BsaWNlLWFtdWxldBpiCkA2YzU4MDJmODY3MDlhMGFkNDc4NGFmODFmMGJhYjQwZjMwNzBiMmY1ODEyOGQ4ODQzZGExZTE3ODRjMTQ3ODAyEgZTcGxpY2USBVJvdW5kGg9PcGVuTWluaW5nUm91bmQi1gNq0wMKTQpLOklEU086OjEyMjBlYmEzNGQxM2FiMjIzZjFhOTMzZmJkZTRlNzYwZGZmOWEzYTk2NTAzMTE1MWIwOTE4Y2E5NzM5NDI0NDA2ZGVkCgoKCGoGCgQKAhgoChAKDjIMMC4wMDUwMDAwMDAwCgsKCSlUKRBciVAGAAoLCgkpVLWWo4lQBgAKDgoMagoKCAoGGIDgi7RZCpIBCo8BaowBChYKFGoSChAKDjIMMC4wMDAwMDAwMDAwChYKFGoSChAKDjIMMC4wMDAwMTkwMjU5ChwKGmoYChAKDjIMMC4wMDAwMDAwMDAwCgQKAloAChYKFGoSChAKDjIMMC4wMDAwMDAwMDAwChAKDjIMMS4wMDAwMDAwMDAwCgUKAxjIAQoFCgMYyAEKBAoCGGQKlAEKkQFqjgEKGgoYMhY0MDAwMDAwMDAwMC4wMDAwMDAwMDAwChAKDjIMMC4wNTAwMDAwMDAwChAKDjIMMC4xNTAwMDAwMDAwChAKDjIMMC4yMDAwMDAwMDAwChIKEDIOMTAwLjAwMDAwMDAwMDAKEAoOMgwwLjYwMDAwMDAwMDAKFAoSUhAKDjIMMi44NTAwMDAwMDAwCg4KDGoKCggKBhiAmJq8BCpJRFNPOjoxMjIwZWJhMzRkMTNhYjIyM2YxYTkzM2ZiZGU0ZTc2MGRmZjlhM2E5NjUwMzExNTFiMDkxOGNhOTczOTQyNDQwNmRlZDlU40w4iVAGAEIqCiYKJAgBEiC5lPXAIb52Vm5sP1M+Kv8KSN9C7Scb2txFp5PgelGv9xAe',
+    created_at: '2026-04-28T18:15:45.568596Z',
+
+    domain_id:
+        'global-domain::1220eba34d13ab223f1a933fbde4e760dff9a3a965031151b0918ca9739424406ded',
+}
+
+export const renewCommand = {
+    choice: 'TransferPreapproval_Renew',
+    choiceArgument: {
+        context: {
+            amuletRules:
+                '00013916345ad89e9318d61d5b3654657d8f9bd14a2e3118bd8ed568389bc4decdca1212202c1ebb5a0bc92ede452b506eb55d463bf89343ed7797d9394b88154f21e4bcee',
+            context: {
+                featuredAppRight: null,
+                issuingMiningRounds: [],
+                openMiningRound:
+                    '009d63476d7323e7369d2c7a8b6f444e0b672388814786fcc7bd042dd7f5b86fdfca1212201666a477e90f00839c553108f86e749e6de3268568fe1fb3b73ff2f8f4d1aba7',
+                validatorRights: [],
+            },
+        },
+        inputs: [
+            {
+                tag: 'InputAmulet',
+                value: 'cid1',
+            },
+            {
+                tag: 'InputAmulet',
+                value: 'cid2',
+            },
+        ],
+        newExpiresAt: '2026-07-28T18:19:16.970Z',
+    },
+    contractId: 'cId',
+    templateId:
+        '6c5802f86709a0ad4784af81f0bab40f3070b2f58128d8843da1e1784c147802:Splice.AmuletRules:TransferPreapproval',
+}
+
+export const devnetTapCommand = {
+    choice: 'AmuletRules_DevNet_Tap',
+    choiceArgument: {
+        amount: '2000',
+        openRound:
+            '009d63476d7323e7369d2c7a8b6f444e0b672388814786fcc7bd042dd7f5b86fdfca1212201666a477e90f00839c553108f86e749e6de3268568fe1fb3b73ff2f8f4d1aba7',
+        receiver: 'receiverParty',
+    },
+    contractId:
+        '00013916345ad89e9318d61d5b3654657d8f9bd14a2e3118bd8ed568389bc4decdca1212202c1ebb5a0bc92ede452b506eb55d463bf89343ed7797d9394b88154f21e4bcee',
+    templateId:
+        '6c5802f86709a0ad4784af81f0bab40f3070b2f58128d8843da1e1784c147802:Splice.AmuletRules:AmuletRules',
+}
